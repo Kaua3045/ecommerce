51f94489aba6c7d621e9baa1ecd43d2d3a3bea6c
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-19 15:43:27 -0300

    feat(exceptions): add the domain exception

---
+++ domain/src/main/java/com/kaua/ecommerce/domain/exceptions/DomainException.java
@@ -0,0 +1,30 @@
+package com.kaua.ecommerce.domain.exceptions;
+
+import java.util.List;
+
+public class DomainException extends NoStackTraceException {
+
+    private final transient List<Error> errors;
+
+    protected DomainException(final List<Error> aErrors) {
+        super("DomainException");
+        this.errors = aErrors;
+    }
+
+    protected DomainException(final String aMessage,final List<Error> aErrors) {
+        super(aMessage);
+        this.errors = aErrors;
+    }
+
+    public static DomainException with(final Error aError) {
+        return new DomainException(List.of(aError));
+    }
+
+    public static DomainException with(final List<Error> aErrors) {
+        return new DomainException(aErrors);
+    }
+
+    public List<Error> getErrors() {
+        return errors;
+    }
+}

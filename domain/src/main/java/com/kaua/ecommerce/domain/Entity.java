48d721bf1cfb10cf7d7eee22d66550a85114bd7f
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-19 15:27:10 -0300

    feat(entity): add the entity abstract class

---
+++ domain/src/main/java/com/kaua/ecommerce/domain/Entity.java
@@ -0,0 +1,29 @@
+package com.kaua.ecommerce.domain;
+
+import java.util.Objects;
+
+public abstract class Entity<ID extends Identifier> {
+
+    private final ID id;
+
+    protected Entity(final ID id) {
+        this.id = Objects.requireNonNull(id, "'id' should not be null");
+    }
+
+    public ID getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Entity<?> entity = (Entity<?>) o;
+        return Objects.equals(getId(), entity.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}

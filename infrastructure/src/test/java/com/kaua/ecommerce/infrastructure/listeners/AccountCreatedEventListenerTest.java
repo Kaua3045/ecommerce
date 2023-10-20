4036d180faeed83acb86528d264ca27d35782b47
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-20 16:04:00 -0300

    test(account-created-event-listener): add the account created event listener reject messa test and success test but one error exists

--- infrastructure/src/test/java/com/kaua/ecommerce/infrastructure/listeners/AccountCreatedEventListenerTest.java
+++ infrastructure/src/test/java/com/kaua/ecommerce/infrastructure/listeners/AccountCreatedEventListenerTest.java
@@ -5,7 +5,10 @@ import com.kaua.ecommerce.application.usecases.customer.create.CreateCustomerCom
 import com.kaua.ecommerce.application.usecases.customer.create.CreateCustomerOutput;
 import com.kaua.ecommerce.application.usecases.customer.create.CreateCustomerUseCase;
 import com.kaua.ecommerce.domain.customer.Customer;
+import com.kaua.ecommerce.domain.utils.CommonErrorMessage;
 import com.kaua.ecommerce.domain.utils.InstantUtils;
+import com.kaua.ecommerce.domain.validation.Error;
+import com.kaua.ecommerce.domain.validation.handler.NotificationHandler;
 import com.kaua.ecommerce.infrastructure.AmqpTest;
 import com.kaua.ecommerce.infrastructure.configurations.annotations.AccountCreatedEvent;
 import com.kaua.ecommerce.infrastructure.configurations.json.Json;
@@ -14,6 +17,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.test.RabbitListenerTestHarness;
 import org.springframework.amqp.rabbit.test.TestRabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -70,7 +74,89 @@ public class AccountCreatedEventListenerTest {
         Assertions.assertNotNull(invocationData);
         Assertions.assertNotNull(invocationData.getArguments());
 
-        final var actualMessage = invocationData.getArguments()[0].toString();
+        final var message = (Message) invocationData.getArguments()[0];
+        final var actualMessage = new String(message.getBody());
+        Assertions.assertEquals(expectedMessage, actualMessage);
+
+        final var cmdCaptor = ArgumentCaptor.forClass(CreateCustomerCommand.class);
+        Mockito.verify(createCustomerUseCase, Mockito.times(1))
+                .execute(cmdCaptor.capture());
+
+        final var actualCommand = cmdCaptor.getValue();
+        Assertions.assertEquals(aAccountCreatedEvent.id(), actualCommand.accountId());
+        Assertions.assertEquals(aAccountCreatedEvent.firstName(), actualCommand.firstName());
+        Assertions.assertEquals(aAccountCreatedEvent.lastName(), actualCommand.lastName());
+        Assertions.assertEquals(aAccountCreatedEvent.email(), actualCommand.email());
+    }
+
+    @Test
+    void givenAnInvalidDuplicateAccountCreatedEvent_whenReceiveMessage_shouldRejectAndPurgeMessage() throws InterruptedException {
+        final var aAccountCreatedEvent = new AccountCreatedEventDummy(
+                "123",
+                "teste",
+                "Testes",
+                "teste.testes@tesss.com",
+                InstantUtils.now()
+        );
+
+        final var expectedMessage = Json.writeValueAsString(aAccountCreatedEvent);
+
+        Mockito.when(createCustomerUseCase.execute(Mockito.any()))
+                .thenReturn(Either.left(NotificationHandler.create(new Error("Customer already exists with this account id"))));
+
+        this.testRabbitTemplate.convertAndSend(queueProperties.getQueue(), expectedMessage);
+
+        final var invocationData =
+                harness.getNextInvocationDataFor(
+                        AccountCreatedEventListener.ACCOUNT_CREATED_LISTENER,
+                        1, TimeUnit.SECONDS);
+
+        Assertions.assertNotNull(invocationData);
+        Assertions.assertNotNull(invocationData.getArguments());
+
+        final var message = (Message) invocationData.getArguments()[0];
+        final var actualMessage = new String(message.getBody());
+        Assertions.assertEquals(expectedMessage, actualMessage);
+
+        final var cmdCaptor = ArgumentCaptor.forClass(CreateCustomerCommand.class);
+        Mockito.verify(createCustomerUseCase, Mockito.times(1))
+                .execute(cmdCaptor.capture());
+
+        final var actualCommand = cmdCaptor.getValue();
+        Assertions.assertEquals(aAccountCreatedEvent.id(), actualCommand.accountId());
+        Assertions.assertEquals(aAccountCreatedEvent.firstName(), actualCommand.firstName());
+        Assertions.assertEquals(aAccountCreatedEvent.lastName(), actualCommand.lastName());
+        Assertions.assertEquals(aAccountCreatedEvent.email(), actualCommand.email());
+    }
+
+    @Test
+    void givenAnInvalidFirstNameLengthAccountCreatedEvent_whenReceiveMessage_thenShouldCreateCustomer() throws InterruptedException {
+        final var aAccountCreatedEvent = new AccountCreatedEventDummy(
+                "123",
+                "teste",
+                "Testes",
+                "teste.testes@tesss.com",
+                InstantUtils.now()
+        );
+
+        final var expectedMessage = Json.writeValueAsString(aAccountCreatedEvent);
+
+        Mockito.when(createCustomerUseCase.execute(Mockito.any()))
+                .thenReturn(Either.left(NotificationHandler
+                        .create(new Error(CommonErrorMessage.nullOrBlank("firstName")))));
+
+        this.testRabbitTemplate.convertAndSend(queueProperties.getQueue(), expectedMessage);
+
+        final var invocationData =
+                harness.getNextInvocationDataFor(
+                        AccountCreatedEventListener.ACCOUNT_CREATED_LISTENER,
+                        1, TimeUnit.SECONDS);
+
+        Assertions.assertNotNull(invocationData);
+        Assertions.assertNotNull(invocationData.getArguments());
+
+        final var message = (Message) invocationData.getArguments()[0];
+        final var actualMessage = new String(message.getBody());
         Assertions.assertEquals(expectedMessage, actualMessage);
 
         final var cmdCaptor = ArgumentCaptor.forClass(CreateCustomerCommand.class);

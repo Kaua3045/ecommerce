3f77c231ad6292ecaa517a52179b3309e195a437
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-20 16:02:45 -0300

    feat(account-created-event-listener): add the if to check isLeft and constains account id already exists error

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/listeners/AccountCreatedEventListener.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/listeners/AccountCreatedEventListener.java
@@ -4,11 +4,14 @@ import com.kaua.ecommerce.application.usecases.customer.create.CreateCustomerCom
 import com.kaua.ecommerce.application.usecases.customer.create.CreateCustomerUseCase;
 import com.kaua.ecommerce.infrastructure.configurations.json.Json;
 import com.kaua.ecommerce.infrastructure.customer.models.CreateCustomerListener;
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.annotation.Profile;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.Objects;
 
 @Component
@@ -17,6 +20,7 @@ public class AccountCreatedEventListener {
 
     public static final String ACCOUNT_CREATED_LISTENER = "AccountCreatedListener";
     private static final String ACCOUNT_CREATED_QUEUE = "${amqp.queues.account-created.queue}";
+    private static final String ACCOUNT_ID_ALREADY_EXISTS = "Customer already exists with this account id";
 
     private final CreateCustomerUseCase createCustomerUseCase;
 
@@ -25,8 +29,8 @@ public class AccountCreatedEventListener {
     }
 
     @RabbitListener(id = ACCOUNT_CREATED_LISTENER, queues = ACCOUNT_CREATED_QUEUE)
-    public void onAccountCreated(@Payload final String message) {
-        final var aAccount = Json.readValue(message, CreateCustomerListener.class);
+    public void onAccountCreated(@Payload final Message message, final Channel channel) throws IOException {
+        final var aAccount = Json.readValue(new String(message.getBody()), CreateCustomerListener.class);
 
         final var aCommand = CreateCustomerCommand.with(
                 aAccount.id(),
@@ -36,9 +40,9 @@ public class AccountCreatedEventListener {
 
         final var aResult = this.createCustomerUseCase.execute(aCommand);
 
-        if (aResult.isLeft()) {
-            // TODO: tratar o caso de erro no futuro
-            return;
+        // TODO: trocar depois para getFirstErrorMessage por exemplo e mandar para uma DDL queue a mesma coisa para os outros erros
+        if (aResult.isLeft() && aResult.getLeft().getErrors().get(0).message().equalsIgnoreCase(ACCOUNT_ID_ALREADY_EXISTS)) {
+            channel.basicReject(message.getMessageProperties().getDeliveryTag(), false);
         }
     }
 }

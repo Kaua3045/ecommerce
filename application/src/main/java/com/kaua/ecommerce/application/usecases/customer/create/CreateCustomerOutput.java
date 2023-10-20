6837a5e455f4692430bde15dd5a086af3437fcde
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-20 12:27:39 -0300

    fix(create-customer-output): change string in from method to customer object

--- application/src/main/java/com/kaua/ecommerce/application/usecases/customer/create/CreateCustomerOutput.java
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/customer/create/CreateCustomerOutput.java
@@ -1,8 +1,10 @@
 package com.kaua.ecommerce.application.usecases.customer.create;
 
+import com.kaua.ecommerce.domain.customer.Customer;
+
 public record CreateCustomerOutput(String id) {
 
-    public static CreateCustomerOutput from(String id) {
-        return new CreateCustomerOutput(id);
+    public static CreateCustomerOutput from(final Customer aCustomer) {
+        return new CreateCustomerOutput(aCustomer.getId().getValue());
     }
 }

c222b37fbf9c513f60449ffd47815f0570606ff1
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-21 10:36:10 -0300

    test(annotation): add the controller test annotation

---
+++ infrastructure/src/test/java/com/kaua/ecommerce/infrastructure/ControllerTest.java
@@ -0,0 +1,23 @@
+package com.kaua.ecommerce.infrastructure;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@ActiveProfiles("test")
+@WebMvcTest
+@AutoConfigureMockMvc(addFilters = false)
+@Import(ObjectMapper.class)
+public @interface ControllerTest {
+
+    @AliasFor(annotation = WebMvcTest.class, attribute = "controllers")
+    Class<?>[] controllers() default {};
+}

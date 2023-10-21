526a35c2122c9d0de12eee1f68e098b76f66cc11
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-21 10:05:26 -0300

    feat(update-customer-cpf-usecase): add the update customer cpf usecase abstract class implementation

---
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/customer/update/cpf/DefaultUpdateCustomerCpfUseCase.java
@@ -0,0 +1,35 @@
+package com.kaua.ecommerce.application.usecases.customer.update.cpf;
+
+import com.kaua.ecommerce.application.either.Either;
+import com.kaua.ecommerce.application.gateways.CustomerGateway;
+import com.kaua.ecommerce.domain.customer.Customer;
+import com.kaua.ecommerce.domain.exceptions.NotFoundException;
+import com.kaua.ecommerce.domain.utils.CpfValidator;
+import com.kaua.ecommerce.domain.validation.Error;
+import com.kaua.ecommerce.domain.validation.handler.NotificationHandler;
+
+import java.util.Objects;
+
+public class DefaultUpdateCustomerCpfUseCase extends UpdateCustomerCpfUseCase {
+
+    private final CustomerGateway customerGateway;
+
+    public DefaultUpdateCustomerCpfUseCase(final CustomerGateway customerGateway) {
+        this.customerGateway = Objects.requireNonNull(customerGateway);
+    }
+
+    @Override
+    public Either<NotificationHandler, UpdateCustomerCpfOutput> execute(UpdateCustomerCpfCommand input) {
+        final var aCustomer = this.customerGateway.findByAccountId(input.accountId())
+                .orElseThrow(NotFoundException.with(Customer.class, input.accountId()));
+
+        if (!CpfValidator.validateCpf(input.cpf())) {
+            return Either.left(NotificationHandler.create(new Error("'cpf' invalid")));
+        }
+
+        final var aCustomerWithCpf = aCustomer.changeCpf(input.cpf());
+        this.customerGateway.update(aCustomerWithCpf);
+
+        return Either.right(UpdateCustomerCpfOutput.from(aCustomerWithCpf));
+    }
+}

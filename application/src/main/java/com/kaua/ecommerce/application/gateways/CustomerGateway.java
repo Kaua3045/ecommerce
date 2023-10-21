5b2ece28034dc5ec6eb187f86250dfc40cff55f1
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-21 09:46:47 -0300

    feat(customer-gateway): add the findByAccountId and update methods

--- application/src/main/java/com/kaua/ecommerce/application/gateways/CustomerGateway.java
+++ application/src/main/java/com/kaua/ecommerce/application/gateways/CustomerGateway.java
@@ -2,9 +2,15 @@ package com.kaua.ecommerce.application.gateways;
 
 import com.kaua.ecommerce.domain.customer.Customer;
 
+import java.util.Optional;
+
 public interface CustomerGateway {
 
     Customer create(Customer aCustomer);
 
-    boolean existsByAccountId(String accountId);
+    boolean existsByAccountId(String aAccountId);
+
+    Optional<Customer> findByAccountId(String aAccountId);
+
+    Customer update(Customer aCustomer);
 }

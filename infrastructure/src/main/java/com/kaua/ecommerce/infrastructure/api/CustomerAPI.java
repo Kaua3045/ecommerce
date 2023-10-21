655ebdfd9b6a481dae3cb41f4515cc306542ee6b
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-21 10:31:10 -0300

    feat(customer-api): add the update customer cpf route method

---
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/api/CustomerAPI.java
@@ -0,0 +1,20 @@
+package com.kaua.ecommerce.infrastructure.api;
+
+import com.kaua.ecommerce.infrastructure.customer.models.UpdateCustomerCpfInput;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = "customers")
+public interface CustomerAPI {
+
+    @PatchMapping(
+            value = "{accountId}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    ResponseEntity<?> updateCustomerCpf(@PathVariable String accountId, @RequestBody UpdateCustomerCpfInput body);
+}

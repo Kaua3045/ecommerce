fa9ab978ecdb3e707cc74a35bf988608f468a54d
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-30 09:27:58 -0300

    feat(customer-api): add the update customer telephone method

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/api/CustomerAPI.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/api/CustomerAPI.java
@@ -1,6 +1,7 @@
 package com.kaua.ecommerce.infrastructure.api;
 
 import com.kaua.ecommerce.infrastructure.customer.models.UpdateCustomerCpfInput;
+import com.kaua.ecommerce.infrastructure.customer.models.UpdateCustomerTelephoneInput;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -17,4 +18,11 @@ public interface CustomerAPI {
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     ResponseEntity<?> updateCustomerCpf(@PathVariable String accountId, @RequestBody UpdateCustomerCpfInput body);
+
+    @PatchMapping(
+            value = "{accountId}/telephone",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    ResponseEntity<?> updateCustomerTelephone(@PathVariable String accountId, @RequestBody UpdateCustomerTelephoneInput body);
 }

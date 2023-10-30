cc7bf4a5918913bbfa028f7d91fc3e2bfd0bd1aa
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-30 20:59:36 -0300

    feat(get-customer-by-account-id-response): add the telephone adapter to format number

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/customer/models/GetCustomerByAccountIdResponse.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/customer/models/GetCustomerByAccountIdResponse.java
@@ -1,6 +1,8 @@
 package com.kaua.ecommerce.infrastructure.customer.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.kaua.ecommerce.infrastructure.adapters.TelephoneAdapterImpl;
 
 public record GetCustomerByAccountIdResponse(
         @JsonProperty("id") String id,
@@ -13,4 +15,32 @@ public record GetCustomerByAccountIdResponse(
         @JsonProperty("created_at") String createdAt,
         @JsonProperty("updated_at") String updatedAt
 ) {
+
+    @JsonIgnore
+    private static final TelephoneAdapterImpl telephoneAdapter = new TelephoneAdapterImpl();
+
+    public static GetCustomerByAccountIdResponse with(
+            final String id,
+            final String accountId,
+            final String firstName,
+            final String lastName,
+            final String email,
+            final String cpf,
+            final String telephone,
+            final String createdAt,
+            final String updatedAt,
+            final String locale
+    ) {
+        return new GetCustomerByAccountIdResponse(
+                id,
+                accountId,
+                firstName,
+                lastName,
+                email,
+                cpf,
+                telephoneAdapter.formatToCountry(telephone, locale),
+                createdAt,
+                updatedAt
+        );
+    }
 }

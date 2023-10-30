dc707ea7c6fee263d4afe6e8cd7742055c124e34
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-30 09:16:50 -0300

    feat(telephone-adapter): add the formatInternational number

--- application/src/main/java/com/kaua/ecommerce/application/adapters/TelephoneAdapter.java
+++ application/src/main/java/com/kaua/ecommerce/application/adapters/TelephoneAdapter.java
@@ -3,4 +3,6 @@ package com.kaua.ecommerce.application.adapters;
 public interface TelephoneAdapter {
 
     boolean validate(String aTelephone);
+
+    String formatInternational(String aTelephone);
 }

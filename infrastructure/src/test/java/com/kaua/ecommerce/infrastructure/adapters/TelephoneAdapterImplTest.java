44093aa5972a0a789a520239987eeb1e1dc3129b
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-30 09:45:13 -0300

    test(telephone-adapter): add the telephone adapter tests

---
+++ infrastructure/src/test/java/com/kaua/ecommerce/infrastructure/adapters/TelephoneAdapterImplTest.java
@@ -0,0 +1,55 @@
+package com.kaua.ecommerce.infrastructure.adapters;
+
+import com.kaua.ecommerce.infrastructure.IntegrationTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@IntegrationTest
+public class TelephoneAdapterImplTest {
+
+    @Autowired
+    private TelephoneAdapterImpl telephoneAdapter;
+
+    @Test
+    void givenAValidTelephone_whenCallValidate_shouldReturnTrue() {
+        final var aTelephone = "+55 (27) 2445-8092";
+
+        final var actual = telephoneAdapter.validate(aTelephone);
+
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    void givenAInvalidTelephone_whenCallValidate_shouldReturnFalse() {
+        final var aTelephone = "+551199999999";
+
+        final var actual = telephoneAdapter.validate(aTelephone);
+
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    void givenAInvalidNullTelephone_whenCallValidate_shouldThrowRuntimeException() {
+        final String aTelephone = null;
+
+        Assertions.assertThrows(RuntimeException.class, () -> telephoneAdapter.validate(aTelephone));
+    }
+
+    @Test
+    void givenAValidTelephone_whenCallFormatInternational_shouldReturnFormattedTelephone() {
+        final var aRawTelephone = "+1 (123) 456-7890";
+        final var aTelephone = "+11234567890";
+
+        final var actual = telephoneAdapter.formatInternational(aRawTelephone);
+
+        Assertions.assertEquals(aTelephone, actual);
+    }
+
+    @Test
+    void givenAInvalidNullTelephone_whenCallFormatInternational_shouldThrowRuntimeException() {
+        final String aTelephone = null;
+
+        Assertions.assertThrows(RuntimeException.class, () -> telephoneAdapter.formatInternational(aTelephone));
+    }
+}

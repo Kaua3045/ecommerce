1c5c273a5548731e57cb53c23264ecffecf6b64f
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-30 19:21:27 -0300

    feat(get-customer-by-account-id-usecase): add the get customer by account id usecase abstract class

---
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/customer/retrieve/get/GetCustomerByAccountIdUseCase.java
@@ -0,0 +1,6 @@
+package com.kaua.ecommerce.application.usecases.customer.retrieve.get;
+
+import com.kaua.ecommerce.application.UseCase;
+
+public abstract class GetCustomerByAccountIdUseCase extends UseCase<GetCustomerByAccountIdOutput, String> {
+}

01d84f921474964ace48b8c9d06ed233475e56cb
Kauã Pereira <zkauacodes@hotmail.com>
2023-10-30 09:21:45 -0300

    feat(migration): add the telephone column

--- infrastructure/src/main/resources/db/migration/V1__CREATE_CUSTOMERS_TABLE.sql
+++ infrastructure/src/main/resources/db/migration/V1__CREATE_CUSTOMERS_TABLE.sql
@@ -5,6 +5,7 @@ CREATE TABLE customers (
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     cpf VARCHAR(12) UNIQUE,
+    telephone VARCHAR(50) UNIQUE,
     created_at DATETIME(6) NOT NULL,
     updated_at DATETIME(6) NOT NULL
 );

639067f3ab209f72b21f785d67457aaaddb495b5
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-02 10:41:30 -0300

    feat(customer-api): add the update customer address method route implementation

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/api/controllers/CustomerController.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/api/controllers/CustomerController.java
@@ -1,11 +1,14 @@
 package com.kaua.ecommerce.infrastructure.api.controllers;
 
 import com.kaua.ecommerce.application.usecases.customer.retrieve.get.GetCustomerByAccountIdUseCase;
+import com.kaua.ecommerce.application.usecases.customer.update.address.UpdateCustomerAddressCommand;
+import com.kaua.ecommerce.application.usecases.customer.update.address.UpdateCustomerAddressUseCase;
 import com.kaua.ecommerce.application.usecases.customer.update.cpf.UpdateCustomerCpfCommand;
 import com.kaua.ecommerce.application.usecases.customer.update.cpf.UpdateCustomerCpfUseCase;
 import com.kaua.ecommerce.application.usecases.customer.update.telephone.UpdateCustomerTelephoneCommand;
 import com.kaua.ecommerce.application.usecases.customer.update.telephone.UpdateCustomerTelephoneUseCase;
 import com.kaua.ecommerce.infrastructure.api.CustomerAPI;
+import com.kaua.ecommerce.infrastructure.customer.models.UpdateCustomerAddressInput;
 import com.kaua.ecommerce.infrastructure.customer.models.UpdateCustomerCpfInput;
 import com.kaua.ecommerce.infrastructure.customer.models.UpdateCustomerTelephoneInput;
 import com.kaua.ecommerce.infrastructure.customer.presenter.CustomerApiPresenter;
@@ -19,15 +22,18 @@ public class CustomerController implements CustomerAPI {
     private final UpdateCustomerCpfUseCase updateCustomerCpfUseCase;
     private final UpdateCustomerTelephoneUseCase updateCustomerTelephoneUseCase;
     private final GetCustomerByAccountIdUseCase getCustomerByAccountIdUseCase;
+    private final UpdateCustomerAddressUseCase updateCustomerAddressUseCase;
 
     public CustomerController(
             final UpdateCustomerCpfUseCase updateCustomerCpfUseCase,
             final UpdateCustomerTelephoneUseCase updateCustomerTelephoneUseCase,
-            final GetCustomerByAccountIdUseCase getCustomerByAccountIdUseCase
+            final GetCustomerByAccountIdUseCase getCustomerByAccountIdUseCase,
+            final UpdateCustomerAddressUseCase updateCustomerAddressUseCase
     ) {
         this.updateCustomerCpfUseCase = updateCustomerCpfUseCase;
         this.updateCustomerTelephoneUseCase = updateCustomerTelephoneUseCase;
         this.getCustomerByAccountIdUseCase = getCustomerByAccountIdUseCase;
+        this.updateCustomerAddressUseCase = updateCustomerAddressUseCase;
     }
 
     @Override
@@ -50,6 +56,25 @@ public class CustomerController implements CustomerAPI {
     public ResponseEntity<?> updateCustomerTelephone(String accountId, UpdateCustomerTelephoneInput body) {
         final var aResult = this.updateCustomerTelephoneUseCase
                 .execute(UpdateCustomerTelephoneCommand.with(accountId, body.telephone()));
+
+        return aResult.isLeft()
+                ? ResponseEntity.unprocessableEntity().body(aResult.getLeft())
+                : ResponseEntity.status(HttpStatus.OK).body(aResult.getRight());
+    }
+
+    @Override
+    public ResponseEntity<?> updateCustomerAddress(String accountId, UpdateCustomerAddressInput body) {
+        final var aResult = this.updateCustomerAddressUseCase
+                .execute(UpdateCustomerAddressCommand.with(
+                        accountId,
+                        body.street(),
+                        body.number(),
+                        body.complement(),
+                        body.district(),
+                        body.city(),
+                        body.state(),
+                        body.zipCode()
+                ));
 
         return aResult.isLeft()
                 ? ResponseEntity.unprocessableEntity().body(aResult.getLeft())

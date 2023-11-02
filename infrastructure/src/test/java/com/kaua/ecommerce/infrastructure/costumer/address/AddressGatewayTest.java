05616edb6c7ce5dc20968e79e0de47cec32c3e6c
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-02 11:01:00 -0300

    test(address-gateway): add the address gateway tests

---
+++ infrastructure/src/test/java/com/kaua/ecommerce/infrastructure/costumer/address/AddressGatewayTest.java
@@ -0,0 +1,52 @@
+package com.kaua.ecommerce.infrastructure.costumer.address;
+
+import com.kaua.ecommerce.domain.customer.address.Address;
+import com.kaua.ecommerce.infrastructure.IntegrationTest;
+import com.kaua.ecommerce.infrastructure.customer.address.AddressMySQLGateway;
+import com.kaua.ecommerce.infrastructure.customer.address.persistence.AddressJpaEntity;
+import com.kaua.ecommerce.infrastructure.customer.address.persistence.AddressJpaRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@IntegrationTest
+public class AddressGatewayTest {
+
+    @Autowired
+    private AddressMySQLGateway addressGateway;
+
+    @Autowired
+    private AddressJpaRepository addressRepository;
+
+    @Test
+    void givenAValidAddressId_whenCallDeleteById_shouldBeOk() {
+        final var aAddress = Address.newAddress(
+                "Rua dos Bobos",
+                "0",
+                "Casa",
+                "Centro",
+                "São Paulo",
+                "SP",
+                "12345678"
+        );
+
+        Assertions.assertEquals(0, addressRepository.count());
+        addressRepository.save(AddressJpaEntity.toEntity(aAddress));
+        Assertions.assertEquals(1, addressRepository.count());
+
+        addressGateway.deleteById(aAddress.getId().getValue());
+
+        Assertions.assertEquals(0, addressRepository.count());
+    }
+
+    @Test
+    void givenAnInvalidAddressId_whenCallDeleteById_shouldBeOk() {
+        final var aAddressId = "123";
+
+        Assertions.assertEquals(0, addressRepository.count());
+
+        addressGateway.deleteById(aAddressId);
+
+        Assertions.assertEquals(0, addressRepository.count());
+    }
+}

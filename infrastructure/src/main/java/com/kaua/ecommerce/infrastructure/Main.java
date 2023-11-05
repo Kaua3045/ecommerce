4f83818df8574b4a6f621eed60053e5ec84d151f
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-05 14:17:14 -0300

    feat(main): add the enable redis repositories configuration

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/Main.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/Main.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableRedisRepositories
 public class Main {
     public static void main(String[] args) {
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");

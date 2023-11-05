b2e03c4fb53c6c1060aaa70fe9c25338d6bbe709
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-05 13:15:46 -0300

    feat(get-customer-by-account-id-usecase): add the cache gateway to retrieve cache

--- application/src/main/java/com/kaua/ecommerce/application/usecases/customer/retrieve/get/DefaultGetCustomerByAccountIdUseCase.java
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/customer/retrieve/get/DefaultGetCustomerByAccountIdUseCase.java
@@ -1,5 +1,6 @@
 package com.kaua.ecommerce.application.usecases.customer.retrieve.get;
 
+import com.kaua.ecommerce.application.gateways.CacheGateway;
 import com.kaua.ecommerce.application.gateways.CustomerGateway;
 import com.kaua.ecommerce.domain.customer.Customer;
 import com.kaua.ecommerce.domain.exceptions.NotFoundException;
@@ -9,15 +10,24 @@ import java.util.Objects;
 public class DefaultGetCustomerByAccountIdUseCase extends GetCustomerByAccountIdUseCase {
 
     private final CustomerGateway customerGateway;
+    private final CacheGateway<Customer> customerCacheGateway;
 
-    public DefaultGetCustomerByAccountIdUseCase(final CustomerGateway customerGateway) {
+    public DefaultGetCustomerByAccountIdUseCase(
+            final CustomerGateway customerGateway,
+            final CacheGateway<Customer> customerCacheGateway
+    ) {
         this.customerGateway = Objects.requireNonNull(customerGateway);
+        this.customerCacheGateway = Objects.requireNonNull(customerCacheGateway);
     }
 
     @Override
     public GetCustomerByAccountIdOutput execute(String aAccountId) {
-        return this.customerGateway.findByAccountId(aAccountId)
+        return this.customerCacheGateway.get(aAccountId)
                 .map(GetCustomerByAccountIdOutput::from)
-                .orElseThrow(NotFoundException.with(Customer.class, aAccountId));
+                .orElseGet(() -> this.customerGateway.findByAccountId(aAccountId)
+                        .map(customer -> {
+                            this.customerCacheGateway.save(customer);
+                            return GetCustomerByAccountIdOutput.from(customer);
+                        }).orElseThrow(NotFoundException.with(Customer.class, aAccountId)));
     }
 }

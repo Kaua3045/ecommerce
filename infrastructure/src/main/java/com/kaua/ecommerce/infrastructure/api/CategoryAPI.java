4fe1163e928525c22e3b0e0a83afbf84e786df20
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-15 13:00:39 -0300

    feat(category-api): add the create category route method

---
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/api/CategoryAPI.java
@@ -0,0 +1,18 @@
+package com.kaua.ecommerce.infrastructure.api;
+
+import com.kaua.ecommerce.infrastructure.category.models.CreateCategoryInput;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = "categories")
+public interface CategoryAPI {
+
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    ResponseEntity<?> createCategory(@RequestBody CreateCategoryInput body);
+}

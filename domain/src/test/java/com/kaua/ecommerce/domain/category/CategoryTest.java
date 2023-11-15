349cfcc4072f5f290cd300ccae9ccb7db33977f7
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-15 17:06:50 -0300

    test(category): fix tests after change category parent to category id parent

--- domain/src/test/java/com/kaua/ecommerce/domain/category/CategoryTest.java
+++ domain/src/test/java/com/kaua/ecommerce/domain/category/CategoryTest.java
@@ -18,7 +18,7 @@ public class CategoryTest {
         final var aName = "Category Name";
         final var aDescription = "Category Description";
         final var aSlug = "category-name";
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var aCategory = Category.newCategory(
                 aName,
@@ -32,7 +32,7 @@ public class CategoryTest {
         Assertions.assertEquals(aName, aCategory.getName());
         Assertions.assertEquals(aDescription, aCategory.getDescription());
         Assertions.assertEquals(aSlug, aCategory.getSlug());
-        Assertions.assertTrue(aCategory.getParent().isEmpty());
+        Assertions.assertTrue(aCategory.getParentId().isEmpty());
         Assertions.assertEquals(0, aCategory.getSubCategories().size());
         Assertions.assertNotNull(aCategory.getCreatedAt());
         Assertions.assertNotNull(aCategory.getUpdatedAt());
@@ -45,7 +45,7 @@ public class CategoryTest {
         final var aName = "Category Name";
         final String aDescription = null;
         final var aSlug = "category-name";
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var aCategory = Category.newCategory(
                 aName,
@@ -59,7 +59,7 @@ public class CategoryTest {
         Assertions.assertEquals(aName, aCategory.getName());
         Assertions.assertNull(aCategory.getDescription());
         Assertions.assertEquals(aSlug, aCategory.getSlug());
-        Assertions.assertTrue(aCategory.getParent().isEmpty());
+        Assertions.assertTrue(aCategory.getParentId().isEmpty());
         Assertions.assertEquals(0, aCategory.getSubCategories().size());
         Assertions.assertNotNull(aCategory.getCreatedAt());
         Assertions.assertNotNull(aCategory.getUpdatedAt());
@@ -84,7 +84,7 @@ public class CategoryTest {
         final String aName = null;
         final String aDescription = null;
         final var aSlug = "category-name";
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var expectedErrorCount = 1;
         final var expectedErrorMessage = CommonErrorMessage.nullOrBlank("name");
@@ -109,7 +109,7 @@ public class CategoryTest {
         final var aName = " ";
         final String aDescription = null;
         final var aSlug = "category-name";
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var expectedErrorCount = 1;
         final var expectedErrorMessage = CommonErrorMessage.nullOrBlank("name");
@@ -134,7 +134,7 @@ public class CategoryTest {
         final var aName = "ca ";
         final String aDescription = null;
         final var aSlug = "category-name";
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var expectedErrorCount = 1;
         final var expectedErrorMessage = CommonErrorMessage.lengthBetween("name", 3, 255);
@@ -159,7 +159,7 @@ public class CategoryTest {
         final var aName = RandomStringUtils.generateValue(256);
         final String aDescription = null;
         final var aSlug = "category-name";
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var expectedErrorCount = 1;
         final var expectedErrorMessage = CommonErrorMessage.lengthBetween("name", 3, 255);
@@ -184,7 +184,7 @@ public class CategoryTest {
         final var aName = "Category Name";
         final var aDescription = " ";
         final var aSlug = "category-name";
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var expectedErrorCount = 1;
         final var expectedErrorMessage = "'description' should not be blank";
@@ -209,7 +209,7 @@ public class CategoryTest {
         final var aName = "Category Name";
         final var aDescription = RandomStringUtils.generateValue(256);
         final var aSlug = "category-name";
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var expectedErrorCount = 1;
         final var expectedErrorMessage = CommonErrorMessage.lengthBetween("description", 0, 255);
@@ -234,7 +234,7 @@ public class CategoryTest {
         final var aName = "Category Name";
         final var aDescription = "Category Description";
         final var aSlug = " ";
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var expectedErrorCount = 1;
         final var expectedErrorMessage = CommonErrorMessage.nullOrBlank("slug");
@@ -259,7 +259,7 @@ public class CategoryTest {
         final var aName = "Category Name";
         final var aDescription = "Category Description";
         final String aSlug = null;
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var expectedErrorCount = 1;
         final var expectedErrorMessage = CommonErrorMessage.nullOrBlank("slug");
@@ -284,7 +284,7 @@ public class CategoryTest {
         final var aName = "Category Name";
         final String aDescription = null;
         final var aSlug = "ca ";
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var expectedErrorCount = 1;
         final var expectedErrorMessage = CommonErrorMessage.lengthBetween("slug", 3, 255);
@@ -309,7 +309,7 @@ public class CategoryTest {
         final var aName = "Category Name";
         final String aDescription = null;
         final var aSlug = RandomStringUtils.generateValue(256);
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var expectedErrorCount = 1;
         final var expectedErrorMessage = CommonErrorMessage.lengthBetween("slug", 3, 255);
@@ -334,7 +334,7 @@ public class CategoryTest {
         final var aName = "Category Name";
         final var aDescription = "Category Description";
         final var aSlug = "category-name";
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var expectedErrorMessage = CommonErrorMessage.lengthBetween("subCategories", 0, 5);
 
@@ -358,7 +358,7 @@ public class CategoryTest {
         final var aName = "Category Name";
         final var aDescription = "Category Description";
         final var aSlug = "category-name";
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var expectedErrorMessage = CommonErrorMessage.lengthBetween("subCategories", 0, 5);
 
@@ -385,7 +385,7 @@ public class CategoryTest {
         final var aName = "Category Name";
         final var aDescription = "Category Description";
         final var aSlug = "category-name";
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var aCategory = Category.newCategory(
                 aName,
@@ -403,7 +403,7 @@ public class CategoryTest {
         Assertions.assertEquals(aName, aCategory.getName());
         Assertions.assertEquals(aDescription, aCategory.getDescription());
         Assertions.assertEquals(aSlug, aCategory.getSlug());
-        Assertions.assertTrue(aCategory.getParent().isEmpty());
+        Assertions.assertTrue(aCategory.getParentId().isEmpty());
         Assertions.assertEquals(5, aCategory.getSubCategories().size());
         Assertions.assertNotNull(aCategory.getCreatedAt());
         Assertions.assertNotNull(aCategory.getUpdatedAt());
@@ -416,7 +416,7 @@ public class CategoryTest {
         final var aName = "Category Name";
         final var aDescription = "Category Description";
         final var aSlug = "category-name";
-        final Category aParent = null;
+        final var aParent = CategoryID.from("123");
 
         final var aCategory = Category.newCategory(
                 aName,
@@ -431,7 +431,7 @@ public class CategoryTest {
                 aName,
                 aDescription,
                 aSlug,
-                aParent,
+                aParent.getValue(),
                 aSubCategories,
                 aCategory.getLevel(),
                 aCategory.getCreatedAt(),
@@ -443,7 +443,7 @@ public class CategoryTest {
         Assertions.assertEquals(aCategory.getName(), aCategoryWith.getName());
         Assertions.assertEquals(aCategory.getDescription(), aCategoryWith.getDescription());
         Assertions.assertEquals(aCategory.getSlug(), aCategoryWith.getSlug());
-        Assertions.assertTrue(aCategoryWith.getParent().isEmpty());
+        Assertions.assertTrue(aCategoryWith.getParentId().isPresent());
         Assertions.assertEquals(5, aCategoryWith.getSubCategories().size());
         Assertions.assertEquals(aCategory.getLevel(), aCategoryWith.getLevel());
         Assertions.assertEquals(aCategory.getCreatedAt(), aCategoryWith.getCreatedAt());
@@ -456,7 +456,7 @@ public class CategoryTest {
         final var aName = "Category Name";
         final var aDescription = "Category Description";
         final var aSlug = "category-name";
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var aCategory = Category.newCategory(
                 aName,
@@ -471,7 +471,7 @@ public class CategoryTest {
                 aName,
                 aDescription,
                 aSlug,
-                aParent,
+                null,
                 aSubCategories,
                 aCategory.getLevel(),
                 aCategory.getCreatedAt(),
@@ -483,7 +483,7 @@ public class CategoryTest {
         Assertions.assertEquals(aCategory.getName(), aCategoryWith.getName());
         Assertions.assertEquals(aCategory.getDescription(), aCategoryWith.getDescription());
         Assertions.assertEquals(aCategory.getSlug(), aCategoryWith.getSlug());
-        Assertions.assertTrue(aCategoryWith.getParent().isEmpty());
+        Assertions.assertTrue(aCategoryWith.getParentId().isEmpty());
         Assertions.assertEquals(0, aCategoryWith.getSubCategories().size());
         Assertions.assertEquals(aCategory.getLevel(), aCategoryWith.getLevel());
         Assertions.assertEquals(aCategory.getCreatedAt(), aCategoryWith.getCreatedAt());
@@ -496,7 +496,7 @@ public class CategoryTest {
         final var aName = "Category Name";
         final var aDescription = "Category Description";
         final var aSlug = "category-name";
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var expectedErrorMessage = CommonErrorMessage.lengthBetween("subCategories", 0, 5);
 
@@ -520,7 +520,7 @@ public class CategoryTest {
         final var aName = "Category Name";
         final var aDescription = "Category Description";
         final var aSlug = "category-name";
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var expectedErrorMessage = CommonErrorMessage.lengthBetween("subCategories", 0, 5);
 
@@ -547,7 +547,7 @@ public class CategoryTest {
         final var aName = "Category Name";
         final var aDescription = "Category Description";
         final var aSlug = "category-name";
-        final Category aParent = null;
+        final CategoryID aParent = null;
 
         final var aCategory = Category.newCategory(
                 aName,
@@ -565,7 +565,7 @@ public class CategoryTest {
         Assertions.assertEquals(aName, aCategory.getName());
         Assertions.assertEquals(aDescription, aCategory.getDescription());
         Assertions.assertEquals(aSlug, aCategory.getSlug());
-        Assertions.assertTrue(aCategory.getParent().isEmpty());
+        Assertions.assertTrue(aCategory.getParentId().isEmpty());
         Assertions.assertEquals(5, aCategory.getSubCategories().size());
         Assertions.assertNotNull(aCategory.getCreatedAt());
         Assertions.assertNotNull(aCategory.getUpdatedAt());

a1cb037c7260c771657c64ac04c6307fbc0e8d3a
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-16 09:43:45 -0300

    feat(category-api): add the update subcategories route method

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/api/CategoryAPI.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/api/CategoryAPI.java
@@ -1,11 +1,10 @@
 package com.kaua.ecommerce.infrastructure.api;
 
 import com.kaua.ecommerce.infrastructure.category.models.CreateCategoryInput;
+import com.kaua.ecommerce.infrastructure.category.models.UpdateSubCategoriesInput;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(value = "categories")
 public interface CategoryAPI {
@@ -15,4 +14,11 @@ public interface CategoryAPI {
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     ResponseEntity<?> createCategory(@RequestBody CreateCategoryInput body);
+
+    @PatchMapping(
+            value = "{id}/sub",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    ResponseEntity<?> updateSubCategories(@PathVariable String id, @RequestBody UpdateSubCategoriesInput body);
 }

f524917e8c92b86af5c4f80719ae5a38df2a91a1
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-16 17:18:21 -0300

    feat(category-jpa-repository): override findById method to load sub categories

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/category/persistence/CategoryJpaRepository.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/category/persistence/CategoryJpaRepository.java
@@ -1,8 +1,17 @@
 package com.kaua.ecommerce.infrastructure.category.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
+
+import java.util.Optional;
 
 public interface CategoryJpaRepository extends JpaRepository<CategoryJpaEntity, String> {
 
     boolean existsByName(String name);
+
+    @Override
+    @NonNull
+    @Query("SELECT c FROM CategoryJpaEntity c LEFT JOIN FETCH c.subCategories WHERE c.id = :id")
+    Optional<CategoryJpaEntity> findById(@NonNull String id);
 }

84261b4e3ec34e3e2d8f4ed07d1853db7fb057c0
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-16 11:07:00 -0300

    feat(customer-api): add the swagger annotations

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/api/CustomerAPI.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/api/CustomerAPI.java
@@ -3,10 +3,15 @@ package com.kaua.ecommerce.infrastructure.api;
 import com.kaua.ecommerce.infrastructure.customer.models.UpdateCustomerAddressInput;
 import com.kaua.ecommerce.infrastructure.customer.models.UpdateCustomerCpfInput;
 import com.kaua.ecommerce.infrastructure.customer.models.UpdateCustomerTelephoneInput;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Customer")
 @RequestMapping(value = "customers")
 public interface CustomerAPI {
 
@@ -14,6 +19,12 @@ public interface CustomerAPI {
             value = "{accountId}",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
+    @Operation(summary = "Get customer by account id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "20", description = "Customer retrieved successfully"),
+            @ApiResponse(responseCode = "404", description = "Customer was not found"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown")
+    })
     ResponseEntity<?> getCustomer(
             @PathVariable String accountId,
             @RequestParam(name = "locale", defaultValue = "BR") String locale);
@@ -23,6 +34,13 @@ public interface CustomerAPI {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
+    @Operation(summary = "Update customer cpf by account id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Customer was not found"),
+            @ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown")
+    })
     ResponseEntity<?> updateCustomerCpf(@PathVariable String accountId, @RequestBody UpdateCustomerCpfInput body);
 
     @PatchMapping(
@@ -30,6 +48,13 @@ public interface CustomerAPI {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
+    @Operation(summary = "Update customer telephone by account id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Customer was not found"),
+            @ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown")
+    })
     ResponseEntity<?> updateCustomerTelephone(@PathVariable String accountId, @RequestBody UpdateCustomerTelephoneInput body);
 
     @PatchMapping(
@@ -37,5 +62,12 @@ public interface CustomerAPI {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
+    @Operation(summary = "Update customer address by account id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Customer was not found"),
+            @ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown")
+    })
     ResponseEntity<?> updateCustomerAddress(@PathVariable String accountId, @RequestBody UpdateCustomerAddressInput body);
 }

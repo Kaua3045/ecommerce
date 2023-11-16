048fd82eb8b0770ef3728fa6deb3ee6a6ddd08bd
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-16 15:57:08 -0300

    test(search-query): add the search query record class test

---
+++ domain/src/test/java/com/kaua/ecommerce/domain/pagination/SearchQueryTest.java
@@ -0,0 +1,24 @@
+package com.kaua.ecommerce.domain.pagination;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SearchQueryTest {
+
+    @Test
+    void givenAValidValues_whenCallNewSearchQuery_shouldReturnASearchQueryInstance() {
+        final var page = 0;
+        final var perPage = 10;
+        final var terms = "";
+        final var sort = "createdAt";
+        final var direction = "asc";
+
+        final var aQuery = new SearchQuery(page, perPage, terms, sort, direction);
+
+        Assertions.assertEquals(page, aQuery.page());
+        Assertions.assertEquals(perPage, aQuery.perPage());
+        Assertions.assertEquals(terms, aQuery.terms());
+        Assertions.assertEquals(sort, aQuery.sort());
+        Assertions.assertEquals(direction, aQuery.direction());
+    }
+}

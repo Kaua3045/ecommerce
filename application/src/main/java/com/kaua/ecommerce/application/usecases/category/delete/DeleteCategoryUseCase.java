f58cfb5511cb2ec8b94d5c4075cc112c1fecd1df
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-16 12:11:11 -0300

    feat(delete-category-usecase): add the delete category usecase abstract class

---
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/category/delete/DeleteCategoryUseCase.java
@@ -0,0 +1,6 @@
+package com.kaua.ecommerce.application.usecases.category.delete;
+
+import com.kaua.ecommerce.application.UnitUseCase;
+
+public abstract class DeleteCategoryUseCase extends UnitUseCase<String> {
+}

ab30d7fb1df2b03257ee72409f54623f7c73bf55
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-16 11:08:53 -0300

    feat(category-api): add the swagger annotations

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/api/CategoryAPI.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/api/CategoryAPI.java
@@ -3,10 +3,15 @@ package com.kaua.ecommerce.infrastructure.api;
 import com.kaua.ecommerce.infrastructure.category.models.CreateCategoryInput;
 import com.kaua.ecommerce.infrastructure.category.models.UpdateCategoryInput;
 import com.kaua.ecommerce.infrastructure.category.models.UpdateSubCategoriesInput;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Category")
 @RequestMapping(value = "categories")
 public interface CategoryAPI {
 
@@ -14,6 +19,12 @@ public interface CategoryAPI {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
+    @Operation(summary = "Create a new root category")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created successfully"),
+            @ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown")
+    })
     ResponseEntity<?> createCategory(@RequestBody CreateCategoryInput body);
 
     @PatchMapping(
@@ -21,6 +32,13 @@ public interface CategoryAPI {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
+    @Operation(summary = "Update sub categories by root category id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Root category was not found"),
+            @ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown")
+    })
     ResponseEntity<?> updateSubCategories(@PathVariable String id, @RequestBody UpdateSubCategoriesInput body);
 
     @PatchMapping(
@@ -28,5 +46,12 @@ public interface CategoryAPI {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
+    @Operation(summary = "Update category by it's identifier")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Category was not found"),
+            @ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown")
+    })
     ResponseEntity<?> updateCategory(@PathVariable String id, @RequestBody UpdateCategoryInput body);
 }

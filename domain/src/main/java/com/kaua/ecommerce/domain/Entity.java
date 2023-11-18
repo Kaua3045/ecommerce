582b6aa04352e6c8867dbf7756cf805953ca0604
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-18 18:30:35 -0300

    feat(entity): add the getDomainEvents method, publishDomainEvent and registerEvent method

--- domain/src/main/java/com/kaua/ecommerce/domain/Entity.java
+++ domain/src/main/java/com/kaua/ecommerce/domain/Entity.java
@@ -1,21 +1,48 @@
 package com.kaua.ecommerce.domain;
 
+import com.kaua.ecommerce.domain.event.DomainEvent;
+import com.kaua.ecommerce.domain.event.DomainEventPublisher;
 import com.kaua.ecommerce.domain.validation.ValidationHandler;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 public abstract class Entity<ID extends Identifier> {
 
     private final ID id;
+    private final List<DomainEvent> domainEvents;
 
-    protected Entity(final ID id) {
+    protected Entity(final ID id, final List<DomainEvent> domainEvents) {
         this.id = Objects.requireNonNull(id, "'id' should not be null");
+        this.domainEvents = new ArrayList<>(domainEvents == null ? Collections.emptyList() : domainEvents);
     }
 
     public abstract void validate(ValidationHandler handler);
 
     public ID getId() {
         return id;
+    }
+
+    public List<DomainEvent> getDomainEvents() {
+        return Collections.unmodifiableList(domainEvents);
+    }
+
+    public void publishDomainEvent(final DomainEventPublisher eventPublisher, final String aTopic) {
+        if (eventPublisher == null) {
+            return;
+        }
+
+        getDomainEvents().forEach(event -> eventPublisher.publish(event, aTopic));
+
+        this.domainEvents.clear();
+    }
+
+    public void registerEvent(final DomainEvent event) {
+        if (event != null) {
+            this.domainEvents.add(event);
+        }
     }
 
     @Override

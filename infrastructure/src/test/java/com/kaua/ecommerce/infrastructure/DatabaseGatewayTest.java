4038913093e548834721c6da8c5ecc7e50c1852c
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-18 19:02:38 -0300

    test(database-gateway-test): add the database gateway test annotation to test mysql gateways

---
+++ infrastructure/src/test/java/com/kaua/ecommerce/infrastructure/DatabaseGatewayTest.java
@@ -0,0 +1,28 @@
+package com.kaua.ecommerce.infrastructure;
+
+import com.kaua.ecommerce.config.JpaCleanUpExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.data.redis.AutoConfigureDataRedis;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@ActiveProfiles("test-integration")
+@ComponentScan(
+        basePackages = "com.kaua.ecommerce",
+        useDefaultFilters = false,
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*MySQLGateway")
+        }
+)
+@DataJpaTest
+@ExtendWith(JpaCleanUpExtension.class)
+@AutoConfigureDataRedis
+public @interface DatabaseGatewayTest {
+}

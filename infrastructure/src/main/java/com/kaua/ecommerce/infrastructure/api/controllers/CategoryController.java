ee3c8826e074a10d0bb5a9eceef72c3eea376cbb
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-18 18:55:54 -0300

    feat(category-api): add the listCategories route method implementation

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/api/controllers/CategoryController.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/api/controllers/CategoryController.java
@@ -3,14 +3,19 @@ package com.kaua.ecommerce.infrastructure.api.controllers;
 import com.kaua.ecommerce.application.usecases.category.create.CreateCategoryRootCommand;
 import com.kaua.ecommerce.application.usecases.category.create.CreateCategoryRootUseCase;
 import com.kaua.ecommerce.application.usecases.category.delete.DeleteCategoryUseCase;
+import com.kaua.ecommerce.application.usecases.category.retrieve.list.ListCategoriesUseCase;
 import com.kaua.ecommerce.application.usecases.category.update.UpdateCategoryCommand;
 import com.kaua.ecommerce.application.usecases.category.update.UpdateCategoryUseCase;
 import com.kaua.ecommerce.application.usecases.category.update.subcategories.UpdateSubCategoriesCommand;
 import com.kaua.ecommerce.application.usecases.category.update.subcategories.UpdateSubCategoriesUseCase;
+import com.kaua.ecommerce.domain.pagination.Pagination;
+import com.kaua.ecommerce.domain.pagination.SearchQuery;
 import com.kaua.ecommerce.infrastructure.api.CategoryAPI;
 import com.kaua.ecommerce.infrastructure.category.models.CreateCategoryInput;
+import com.kaua.ecommerce.infrastructure.category.models.ListCategoriesResponse;
 import com.kaua.ecommerce.infrastructure.category.models.UpdateCategoryInput;
 import com.kaua.ecommerce.infrastructure.category.models.UpdateSubCategoriesInput;
+import com.kaua.ecommerce.infrastructure.category.presenter.CategoryApiPresenter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,17 +27,20 @@ public class CategoryController implements CategoryAPI {
     private final UpdateSubCategoriesUseCase updateSubCategoriesUseCase;
     private final UpdateCategoryUseCase updateCategoryUseCase;
     private final DeleteCategoryUseCase deleteCategoryUseCase;
+    private final ListCategoriesUseCase listCategoriesUseCase;
 
     public CategoryController(
             final CreateCategoryRootUseCase createCategoryRootUseCase,
             final UpdateSubCategoriesUseCase updateSubCategoriesUseCase,
             final UpdateCategoryUseCase updateCategoryUseCase,
-            final DeleteCategoryUseCase deleteCategoryUseCase
+            final DeleteCategoryUseCase deleteCategoryUseCase,
+            final ListCategoriesUseCase listCategoriesUseCase
     ) {
         this.createCategoryRootUseCase = createCategoryRootUseCase;
         this.updateSubCategoriesUseCase = updateSubCategoriesUseCase;
         this.updateCategoryUseCase = updateCategoryUseCase;
         this.deleteCategoryUseCase = deleteCategoryUseCase;
+        this.listCategoriesUseCase = listCategoriesUseCase;
     }
 
     @Override
@@ -43,6 +51,13 @@ public class CategoryController implements CategoryAPI {
         return aResult.isLeft()
                 ? ResponseEntity.unprocessableEntity().body(aResult.getLeft())
                 : ResponseEntity.status(HttpStatus.CREATED).body(aResult.getRight());
+    }
+
+    @Override
+    public Pagination<ListCategoriesResponse> listCategories(String search, int page, int perPage, String sort, String direction) {
+        final var aQuery = new SearchQuery(page, perPage, search, sort, direction);
+        return this.listCategoriesUseCase.execute(aQuery)
+                .map(CategoryApiPresenter::present);
     }
 
     @Override

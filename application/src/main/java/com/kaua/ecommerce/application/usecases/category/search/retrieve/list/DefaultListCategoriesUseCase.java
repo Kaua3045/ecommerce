4c5681b9d9d47c19a3889fe55b1543528b43924b
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-20 14:31:59 -0300

    refactor(list-categories-usecase): move to search package

--- application/src/main/java/com/kaua/ecommerce/application/usecases/category/search/retrieve/list/DefaultListCategoriesUseCase.java
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/category/search/retrieve/list/DefaultListCategoriesUseCase.java
@@ -1,4 +1,4 @@
-package com.kaua.ecommerce.application.usecases.category.retrieve.list;
+package com.kaua.ecommerce.application.usecases.category.search.retrieve.list;
 
 import com.kaua.ecommerce.application.gateways.SearchGateway;
 import com.kaua.ecommerce.domain.category.Category;

8a440d487806b1d5eb06872e04d803eebac84fed
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-29 17:54:08 -0300

    fix(update-category-command): fix update category command, add sub category id to update sub category and send correct event

--- application/src/main/java/com/kaua/ecommerce/application/usecases/category/update/DefaultUpdateCategoryUseCase.java
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/category/update/DefaultUpdateCategoryUseCase.java
@@ -3,12 +3,14 @@ package com.kaua.ecommerce.application.usecases.category.update;
 import com.kaua.ecommerce.application.either.Either;
 import com.kaua.ecommerce.application.gateways.CategoryGateway;
 import com.kaua.ecommerce.domain.category.Category;
+import com.kaua.ecommerce.domain.category.events.CategoryUpdatedEvent;
 import com.kaua.ecommerce.domain.exceptions.NotFoundException;
 import com.kaua.ecommerce.domain.utils.SlugUtils;
 import com.kaua.ecommerce.domain.validation.Error;
 import com.kaua.ecommerce.domain.validation.handler.NotificationHandler;
 
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class DefaultUpdateCategoryUseCase extends UpdateCategoryUseCase {
 
@@ -26,21 +28,70 @@ public class DefaultUpdateCategoryUseCase extends UpdateCategoryUseCase {
             return Either.left(aNotification.append(new Error("Category already exists")));
         }
 
-        final var aCategory = this.categoryGateway.findById(input.id())
-                .orElseThrow(NotFoundException.with(Category.class, input.id()));
+        final var aRootCategory = getRootCategory(input.rootCategoryId());
 
-        final var aName = validateName(input, aCategory);
+        if (input.subCategoryId().isPresent()) {
+            final var aSubCategoryUpdated = aRootCategory.getSubCategories().stream().map(aSubCategory -> {
+                if (aSubCategory.getId().getValue().equals(input.subCategoryId().get())) {
+                    final var aSubUpdated = updateCategory(aSubCategory, input);
+                    aSubUpdated.validate(aNotification);
+                    return aSubUpdated;
+                } else {
+                    final var aSubSubCategoryUpdated = aSubCategory.getSubCategories().stream()
+                            .filter(aSubSubCategory -> aSubSubCategory.getId().getValue().equals(input.subCategoryId().get()))
+                            .findFirst()
+                            .map(aSubSubCategory -> {
+                                final var aSubSubUpdated = updateCategory(aSubSubCategory, input);
+                                aSubSubUpdated.validate(aNotification);
+                                return aSubSubUpdated;
+                            });
+                    if (aSubSubCategoryUpdated.isPresent()) {
+                        aSubCategory.addSubCategory(aSubSubCategoryUpdated.get());
+                        return aSubCategory;
+                    }
+                }
+                return aSubCategory;
+            }).collect(Collectors.toSet());
+
+            if (aNotification.hasError()) {
+                return Either.left(aNotification);
+            }
+
+            aRootCategory.addSubCategories(aSubCategoryUpdated);
+
+            return Either.right(updateCategoryAndOutput(aRootCategory));
+        }
+
+        final var aName = validateName(input, aRootCategory);
         final var aSlug = nameToSlug(aName);
-        final var aCategoryUpdated = aCategory.update(aName, input.description(), aSlug);
+        final var aCategoryUpdated = aRootCategory.update(aName, input.description(), aSlug);
         aCategoryUpdated.validate(aNotification);
 
         if (aNotification.hasError()) {
             return Either.left(aNotification);
         }
 
+        aCategoryUpdated.registerEvent(CategoryUpdatedEvent.from(aCategoryUpdated));
         this.categoryGateway.update(aCategoryUpdated);
 
         return Either.right(UpdateCategoryOutput.from(aCategoryUpdated));
+    }
+
+    private Category getRootCategory(final String aRootCategoryId) {
+        return this.categoryGateway.findById(aRootCategoryId)
+                .orElseThrow(NotFoundException.with(Category.class, aRootCategoryId));
+    }
+
+    private Category updateCategory(final Category actualCategory, final UpdateCategoryCommand aInput) {
+        final var aName = validateName(aInput, actualCategory);
+        final var aSlug = nameToSlug(aName);
+        return actualCategory.update(aName, aInput.description(), aSlug);
+    }
+
+    public UpdateCategoryOutput updateCategoryAndOutput(final Category aCategory) {
+        aCategory.registerEvent(CategoryUpdatedEvent.from(aCategory));
+        this.categoryGateway.update(aCategory);
+        return UpdateCategoryOutput.from(aCategory);
     }
 
     private String validateName(final UpdateCategoryCommand input, final Category aOldCategoryName) {

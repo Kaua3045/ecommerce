d8fc803981c002690d92a41ff815da6c620abde5
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-29 17:43:19 -0300

    feat(events-types): add the category updated and category deleted events types

--- domain/src/main/java/com/kaua/ecommerce/domain/event/EventsTypes.java
+++ domain/src/main/java/com/kaua/ecommerce/domain/event/EventsTypes.java
@@ -3,6 +3,8 @@ package com.kaua.ecommerce.domain.event;
 public final class EventsTypes {
 
     public static final String CATEGORY_CREATED = "category_created";
+    public static final String CATEGORY_UPDATED = "category_updated";
+    public static final String CATEGORY_DELETED = "category_deleted";
 
     private EventsTypes() {}
 }

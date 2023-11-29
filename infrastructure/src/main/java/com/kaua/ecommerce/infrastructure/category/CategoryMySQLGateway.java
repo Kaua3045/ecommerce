4b921c9de9ebaa6b752a2c53d6b46a84c4fc1b0e
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-29 18:03:24 -0300

    feat(category-gateway): add the delete root category by id method implementation and event database service to publish domain events in database

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/category/CategoryMySQLGateway.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/category/CategoryMySQLGateway.java
@@ -2,11 +2,10 @@ package com.kaua.ecommerce.infrastructure.category;
 
 import com.kaua.ecommerce.application.gateways.CategoryGateway;
 import com.kaua.ecommerce.domain.category.Category;
+import com.kaua.ecommerce.domain.category.events.CategoryDeletedEvent;
 import com.kaua.ecommerce.infrastructure.category.persistence.CategoryJpaEntity;
 import com.kaua.ecommerce.infrastructure.category.persistence.CategoryJpaRepository;
-import com.kaua.ecommerce.infrastructure.configurations.annotations.CategoryEvents;
-import com.kaua.ecommerce.infrastructure.configurations.properties.kafka.KafkaTopicProperty;
-import com.kaua.ecommerce.infrastructure.service.EventService;
+import com.kaua.ecommerce.infrastructure.service.EventDatabaseService;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,24 +16,21 @@ import java.util.Optional;
 public class CategoryMySQLGateway implements CategoryGateway {
 
     private final CategoryJpaRepository categoryJpaRepository;
-    private final EventService eventService;
-    private final KafkaTopicProperty categoryTopic;
+    private final EventDatabaseService eventDatabaseService;
 
     public CategoryMySQLGateway(
             final CategoryJpaRepository categoryJpaRepository,
-            final EventService eventService,
-            final @CategoryEvents KafkaTopicProperty categoryTopic
+            final EventDatabaseService eventDatabaseService
     ) {
         this.categoryJpaRepository = Objects.requireNonNull(categoryJpaRepository);
-        this.eventService = Objects.requireNonNull(eventService);
-        this.categoryTopic = Objects.requireNonNull(categoryTopic);
+        this.eventDatabaseService = Objects.requireNonNull(eventDatabaseService);
     }
 
     @Transactional
     @Override
     public Category create(Category aCategory) {
         this.categoryJpaRepository.save(CategoryJpaEntity.toEntity(aCategory));
-        aCategory.publishDomainEvent(this.eventService::send, categoryTopic.getTopicName());
+        aCategory.publishDomainEvent(this.eventDatabaseService::send);
         return aCategory;
     }
 
@@ -49,9 +45,11 @@ public class CategoryMySQLGateway implements CategoryGateway {
         return this.categoryJpaRepository.findById(aId).map(CategoryJpaEntity::toDomain);
     }
 
+    @Transactional
     @Override
     public Category update(Category aCategory) {
         this.categoryJpaRepository.save(CategoryJpaEntity.toEntity(aCategory));
+        aCategory.publishDomainEvent(this.eventDatabaseService::send);
         return aCategory;
     }
 
@@ -61,5 +59,19 @@ public class CategoryMySQLGateway implements CategoryGateway {
         if (this.categoryJpaRepository.existsById(aId)) {
             this.categoryJpaRepository.deleteById(aId);
         }
+    }
+
+    @Transactional
+    @Override
+    public void deleteRootCategoryById(String aId) {
+        if (this.categoryJpaRepository.existsById(aId)) {
+            publishRootCategoryDeletedEvent(aId);
+            this.categoryJpaRepository.deleteById(aId);
+        }
+    }
+
+    private void publishRootCategoryDeletedEvent(final String aId) {
+        final var aCategoryDeletedEvent = CategoryDeletedEvent.from(aId, null);
+        this.eventDatabaseService.send(aCategoryDeletedEvent, null);
     }
 }

c6f12bd57ce354ffe788f1d59b775e77a9763980
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-29 17:52:14 -0300

    fix(update-sub-categories-usecase): fix on update sub sub categories and update sub categories

--- application/src/main/java/com/kaua/ecommerce/application/usecases/category/update/subcategories/DefaultUpdateSubCategoriesUseCase.java
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/category/update/subcategories/DefaultUpdateSubCategoriesUseCase.java
@@ -3,12 +3,16 @@ package com.kaua.ecommerce.application.usecases.category.update.subcategories;
 import com.kaua.ecommerce.application.either.Either;
 import com.kaua.ecommerce.application.gateways.CategoryGateway;
 import com.kaua.ecommerce.domain.category.Category;
+import com.kaua.ecommerce.domain.category.CategoryID;
+import com.kaua.ecommerce.domain.category.events.CategoryUpdatedEvent;
 import com.kaua.ecommerce.domain.exceptions.NotFoundException;
 import com.kaua.ecommerce.domain.utils.SlugUtils;
 import com.kaua.ecommerce.domain.validation.Error;
 import com.kaua.ecommerce.domain.validation.handler.NotificationHandler;
 
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DefaultUpdateSubCategoriesUseCase extends UpdateSubCategoriesUseCase {
 
@@ -26,27 +30,137 @@ public class DefaultUpdateSubCategoriesUseCase extends UpdateSubCategoriesUseCas
             return Either.left(aNotification.append(new Error("Category already exists")));
         }
 
-        final var aCategoryRoot = this.categoryGateway.findById(input.id())
-                .orElseThrow(NotFoundException.with(Category.class, input.id()));
+        final var aCategoryRoot = getRootCategory(input.rootCategoryId());
 
         final var aSlug = nameToSlug(input.name());
 
-        final var aSubCategory = Category.newCategory(
-                input.name(),
-                input.description(),
-                aSlug,
-                aCategoryRoot.getId()
-        );
+        Category aSubCategory;
+
+        if (input.subCategoryId().isPresent()) {
+            aSubCategory = createSubCategory(input, aSlug, aCategoryRoot);
+            aSubCategory.validate(aNotification);
+
+            if (aNotification.hasError()) {
+                return Either.left(aNotification);
+            }
+
+            updateSubCategory(input.subCategoryId().get(), aCategoryRoot, aSubCategory);
+
+            return Either.right(updateCategoryAndOutput(aCategoryRoot));
+        }
+
+        aSubCategory = createSubCategory(input, aSlug, aCategoryRoot);
         aSubCategory.validate(aNotification);
 
         if (aNotification.hasError()) {
             return Either.left(aNotification);
         }
 
+        addSubCategoryAndUpdate(aCategoryRoot, aSubCategory);
+
+        return Either.right(updateCategoryAndOutput(aCategoryRoot));
+
+
+//        if (input.subCategoryId().isPresent()) {
+//            aSubCategory = Category.newCategory(
+//                    input.name(),
+//                    input.description(),
+//                    aSlug,
+//                    CategoryID.from(input.subCategoryId().get())
+//            );
+//            aSubCategory.validate(aNotification);
+//
+//            if (aNotification.hasError()) {
+//                return Either.left(aNotification);
+//            }
+//            final var aSubCategoryId = input.subCategoryId().get();
+//            final var aSubCategoryUpdated = aCategoryRoot.getSubCategories().stream()
+//                    .map(aSubCategories -> {
+//                        if (aSubCategories.getId().getValue().equals(aSubCategoryId)) {
+//                            aSubCategories.addSubCategory(aSubCategory);
+//                            aSubCategories.updateSubCategoriesLevel();
+//                            return aSubCategories;
+//                        }
+//                        throw NotFoundException.with(Category.class, aSubCategoryId).get();
+//                    }).collect(Collectors.toSet());
+//
+//            aCategoryRoot.addSubCategories(aSubCategoryUpdated);
+//            aCategoryRoot.registerEvent(CategoryUpdatedEvent.from(aCategoryRoot));
+//            return Either.right(UpdateSubCategoriesOutput.from(this.categoryGateway.update(aCategoryRoot)));
+//        } else {
+//            aSubCategory = Category.newCategory(
+//                    input.name(),
+//                    input.description(),
+//                    aSlug,
+//                    aCategoryRoot.getId()
+//            );
+//            aSubCategory.validate(aNotification);
+//
+//            if (aNotification.hasError()) {
+//                return Either.left(aNotification);
+//            }
+//        }
+//
+//        aCategoryRoot.addSubCategory(aSubCategory);
+//        aCategoryRoot.updateSubCategoriesLevel();
+//        aCategoryRoot.registerEvent(CategoryUpdatedEvent.from(aCategoryRoot));
+//
+//        return Either.right(UpdateSubCategoriesOutput.from(this.categoryGateway.update(aCategoryRoot)));
+    }
+
+    private Category getRootCategory(final String aRootCategoryId) {
+        return this.categoryGateway.findById(aRootCategoryId)
+                .orElseThrow(NotFoundException.with(Category.class, aRootCategoryId));
+    }
+
+    private Category createSubCategory(
+            final UpdateSubCategoriesCommand aInput,
+            final String aSlug,
+            final Category aCategoryRoot
+    ) {
+        if (aInput.subCategoryId().isPresent()) {
+            return Category.newCategory(
+                    aInput.name(),
+                    aInput.description(),
+                    aSlug,
+                    CategoryID.from(aInput.subCategoryId().get())
+            );
+        } else {
+            return Category.newCategory(
+                    aInput.name(),
+                    aInput.description(),
+                    aSlug,
+                    aCategoryRoot.getId()
+            );
+        }
+    }
+
+    private void updateSubCategory(
+            final String aSubCategoryId,
+            final Category aCategoryRoot,
+            final Category aSubCategory) {
+        Set<Category> updatedSubCategories = aCategoryRoot.getSubCategories().stream()
+                .map(subCategories -> {
+                    if (subCategories.getId().getValue().equals(aSubCategoryId)) {
+                        subCategories.addSubCategory(aSubCategory);
+                        subCategories.updateSubCategoriesLevel();
+                        return subCategories;
+                    }
+                    throw NotFoundException.with(Category.class, aSubCategoryId).get();
+                }).collect(Collectors.toSet());
+
+        aCategoryRoot.addSubCategories(updatedSubCategories);
+        aCategoryRoot.registerEvent(CategoryUpdatedEvent.from(aCategoryRoot));
+    }
+
+    private void addSubCategoryAndUpdate(final Category aCategoryRoot, final Category aSubCategory) {
         aCategoryRoot.addSubCategory(aSubCategory);
         aCategoryRoot.updateSubCategoriesLevel();
+        aCategoryRoot.registerEvent(CategoryUpdatedEvent.from(aCategoryRoot));
+    }
 
-        return Either.right(UpdateSubCategoriesOutput.from(this.categoryGateway.update(aCategoryRoot)));
+    private UpdateSubCategoriesOutput updateCategoryAndOutput(final Category aCategoryRoot) {
+        return UpdateSubCategoriesOutput.from(this.categoryGateway.update(aCategoryRoot));
     }
 
     private String nameToSlug(final String aName) {

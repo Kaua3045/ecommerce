e92b858957c35e2a6624bd3777369e8d6dd757d1
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-30 10:08:30 -0300

    test(usecase-test): add the unit test tag

--- application/src/test/java/com/kaua/ecommerce/application/UseCaseTest.java
+++ application/src/test/java/com/kaua/ecommerce/application/UseCaseTest.java
@@ -1,8 +1,10 @@
 package com.kaua.ecommerce.application;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
+@Tag("unitTest")
 public abstract class UseCaseTest {
 }

fe47bb8a10d69b60a609c1700569eb90e1b5d33d
Kauã Pereira <zkauacodes@hotmail.com>
2023-11-30 10:09:34 -0300

    test(create-category-root-usecase): remove unnecessary tests

--- infrastructure/src/test/java/com/kaua/ecommerce/application/category/create/CreateCategoryRootUseCaseIT.java
+++ infrastructure/src/test/java/com/kaua/ecommerce/application/category/create/CreateCategoryRootUseCaseIT.java
@@ -4,7 +4,6 @@ import com.kaua.ecommerce.application.usecases.category.create.CreateCategoryRoo
 import com.kaua.ecommerce.application.usecases.category.create.CreateCategoryRootUseCase;
 import com.kaua.ecommerce.domain.category.Category;
 import com.kaua.ecommerce.domain.utils.CommonErrorMessage;
-import com.kaua.ecommerce.domain.utils.RandomStringUtils;
 import com.kaua.ecommerce.domain.utils.SlugUtils;
 import com.kaua.ecommerce.infrastructure.AbstractEmbeddedKafkaTest;
 import com.kaua.ecommerce.infrastructure.category.persistence.CategoryJpaEntity;
@@ -39,33 +38,6 @@ public class CreateCategoryRootUseCaseIT extends AbstractEmbeddedKafkaTest {
         Assertions.assertNotNull(aCategory.getId());
         Assertions.assertEquals(aName, aCategory.getName());
         Assertions.assertEquals(aDescription, aCategory.getDescription());
-        Assertions.assertEquals(aExpectedSlug, aCategory.getSlug());
-        Assertions.assertNull(aCategory.getParentId());
-        Assertions.assertEquals(0, aCategory.getSubCategoriesLevel());
-        Assertions.assertEquals(0, aCategory.getSubCategories().size());
-        Assertions.assertNotNull(aCategory.getCreatedAt());
-        Assertions.assertNotNull(aCategory.getUpdatedAt());
-        Assertions.assertEquals(1, this.categoryRepository.count());
-    }
-
-    @Test
-    void givenAValidValuesWithoutDescription_whenCallsCreateCategoryRootUseCase_thenCategoryShouldBeCreated() {
-        final var aName = "Category Test";
-        final String aDescription = null;
-        final var aExpectedSlug = SlugUtils.createSlug(aName);
-
-        final var aCommand = CreateCategoryRootCommand.with(aName, aDescription);
-
-        final var aResult = this.createCategoryRootUseCase.execute(aCommand).getRight();
-
-        Assertions.assertNotNull(aResult);
-        Assertions.assertNotNull(aResult.id());
-
-        final var aCategory = this.categoryRepository.findById(aResult.id()).get();
-
-        Assertions.assertNotNull(aCategory.getId());
-        Assertions.assertEquals(aName, aCategory.getName());
-        Assertions.assertNull(aCategory.getDescription());
         Assertions.assertEquals(aExpectedSlug, aCategory.getSlug());
         Assertions.assertNull(aCategory.getParentId());
         Assertions.assertEquals(0, aCategory.getSubCategoriesLevel());
@@ -116,80 +88,6 @@ public class CreateCategoryRootUseCaseIT extends AbstractEmbeddedKafkaTest {
         Assertions.assertEquals(expectedErrorCount, aResult.getErrors().size());
         Assertions.assertEquals(expectedErrorMessageOne, aResult.getErrors().get(0).message());
         Assertions.assertEquals(expectedErrorMessageTwo, aResult.getErrors().get(1).message());
-        Assertions.assertEquals(0, this.categoryRepository.count());
-    }
-
-    @Test
-    void givenAnInvalidBlankNameAndBlankSlug_whenCallsCreateCategoryRootUseCase_shouldReturnDomainException() {
-        final var aName = " ";
-        final var aDescription = "Category Test Description";
-
-        final var expectedErrorMessageOne = CommonErrorMessage.nullOrBlank("name");
-        final var expectedErrorMessageTwo = CommonErrorMessage.nullOrBlank("slug");
-        final var expectedErrorCount = 2;
-
-        final var aCommand = CreateCategoryRootCommand.with(aName, aDescription);
-
-        final var aResult = this.createCategoryRootUseCase.execute(aCommand).getLeft();
-
-        Assertions.assertEquals(expectedErrorCount, aResult.getErrors().size());
-        Assertions.assertEquals(expectedErrorMessageOne, aResult.getErrors().get(0).message());
-        Assertions.assertEquals(expectedErrorMessageTwo, aResult.getErrors().get(1).message());
-        Assertions.assertEquals(0, this.categoryRepository.count());
-    }
-
-    @Test
-    void givenAnInvalidNameLengthLessThan3AndSlugLengthLessThan3_whenCallsCreateCategoryRootUseCase_shouldReturnDomainException() {
-        final var aName = "Ca ";
-        final var aDescription = "Category Test Description";
-
-        final var expectedErrorMessageOne = CommonErrorMessage.lengthBetween("name", 3, 255);
-        final var expectedErrorMessageTwo = CommonErrorMessage.lengthBetween("slug", 3, 255);
-        final var expectedErrorCount = 2;
-
-        final var aCommand = CreateCategoryRootCommand.with(aName, aDescription);
-
-        final var aResult = this.createCategoryRootUseCase.execute(aCommand).getLeft();
-
-        Assertions.assertEquals(expectedErrorCount, aResult.getErrors().size());
-        Assertions.assertEquals(expectedErrorMessageOne, aResult.getErrors().get(0).message());
-        Assertions.assertEquals(expectedErrorMessageTwo, aResult.getErrors().get(1).message());
-        Assertions.assertEquals(0, this.categoryRepository.count());
-    }
-
-    @Test
-    void givenAnInvalidNameLengthMoreThan255AndSlugLengthMoreThan255_whenCallsCreateCategoryRootUseCase_shouldReturnDomainException() {
-        final var aName = RandomStringUtils.generateValue(256);
-        final var aDescription = "Category Test Description";
-
-        final var expectedErrorMessageOne = CommonErrorMessage.lengthBetween("name", 3, 255);
-        final var expectedErrorMessageTwo = CommonErrorMessage.lengthBetween("slug", 3, 255);
-        final var expectedErrorCount = 2;
-
-        final var aCommand = CreateCategoryRootCommand.with(aName, aDescription);
-
-        final var aResult = this.createCategoryRootUseCase.execute(aCommand).getLeft();
-
-        Assertions.assertEquals(expectedErrorCount, aResult.getErrors().size());
-        Assertions.assertEquals(expectedErrorMessageOne, aResult.getErrors().get(0).message());
-        Assertions.assertEquals(expectedErrorMessageTwo, aResult.getErrors().get(1).message());
-        Assertions.assertEquals(0, this.categoryRepository.count());
-    }
-
-    @Test
-    void givenAnInvalidDescriptionLengthMoreThan255_whenCallsCreateCategoryRootUseCase_shouldReturnDomainException() {
-        final var aName = "Category Test";
-        final var aDescription = RandomStringUtils.generateValue(256);
-
-        final var expectedErrorMessage = CommonErrorMessage.lengthBetween("description", 0, 255);
-        final var expectedErrorCount = 1;
-
-        final var aCommand = CreateCategoryRootCommand.with(aName, aDescription);
-
-        final var aResult = this.createCategoryRootUseCase.execute(aCommand).getLeft();
-
-        Assertions.assertEquals(expectedErrorCount, aResult.getErrors().size());
-        Assertions.assertEquals(expectedErrorMessage, aResult.getErrors().get(0).message());
         Assertions.assertEquals(0, this.categoryRepository.count());
     }
 }

9c83c8ab2d44eaa33fa2c7fb08f374212a905e19
Kauã Pereira <zkauacodes@hotmail.com>
2023-12-06 15:28:25 -0300

    test(amqp-test): add the dirties context annotation

--- infrastructure/src/test/java/com/kaua/ecommerce/infrastructure/AmqpTest.java
+++ infrastructure/src/test/java/com/kaua/ecommerce/infrastructure/AmqpTest.java
@@ -3,6 +3,7 @@ package com.kaua.ecommerce.infrastructure;
 import com.kaua.ecommerce.config.AmqpTestConfiguration;
 import org.junit.jupiter.api.Tag;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.lang.annotation.*;
@@ -11,7 +12,8 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @ActiveProfiles("test-integration")
-@SpringBootTest(classes = { Main.class, AmqpTestConfiguration.class })
+@SpringBootTest(classes = {Main.class, AmqpTestConfiguration.class})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @Tag("integrationTest")
 public @interface AmqpTest {
 }

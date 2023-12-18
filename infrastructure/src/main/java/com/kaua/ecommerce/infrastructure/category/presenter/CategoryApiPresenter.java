65bee7614bdba77b8d414fd331e047ab52e1a5a5
Kauã Pereira <zkauacodes@hotmail.com>
2023-12-18 19:56:19 -0300

    feat(category-api-presenter): add the category api presenter get category response

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/category/presenter/CategoryApiPresenter.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/category/presenter/CategoryApiPresenter.java
@@ -1,13 +1,16 @@
 package com.kaua.ecommerce.infrastructure.category.presenter;
 
+import com.kaua.ecommerce.application.usecases.category.search.retrieve.get.GetCategoryByIdOutput;
 import com.kaua.ecommerce.application.usecases.category.search.retrieve.list.ListCategoriesOutput;
+import com.kaua.ecommerce.infrastructure.category.models.GetCategoryResponse;
 import com.kaua.ecommerce.infrastructure.category.models.ListCategoriesResponse;
 
 import java.util.stream.Collectors;
 
 public final class CategoryApiPresenter {
 
-    private CategoryApiPresenter() {}
+    private CategoryApiPresenter() {
+    }
 
     public static ListCategoriesResponse present(final ListCategoriesOutput aOutput) {
         return new ListCategoriesResponse(
@@ -16,6 +19,21 @@ public final class CategoryApiPresenter {
                 aOutput.description(),
                 aOutput.slug(),
                 aOutput.parentId(),
+                aOutput.subCategories().stream()
+                        .map(CategoryApiPresenter::present).collect(Collectors.toSet()),
+                aOutput.createdAt(),
+                aOutput.updatedAt()
+        );
+    }
+
+    public static GetCategoryResponse present(final GetCategoryByIdOutput aOutput) {
+        return new GetCategoryResponse(
+                aOutput.id(),
+                aOutput.name(),
+                aOutput.description(),
+                aOutput.slug(),
+                aOutput.parentId(),
+                aOutput.subCategoriesLevel(),
                 aOutput.subCategories().stream()
                         .map(CategoryApiPresenter::present).collect(Collectors.toSet()),
                 aOutput.createdAt(),

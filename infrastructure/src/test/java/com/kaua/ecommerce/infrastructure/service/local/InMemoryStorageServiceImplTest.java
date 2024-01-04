7bebbac870f6cfd8ec767432ff68221ef1816ea8
Kauã Pereira <zkauacodes@hotmail.com>
2024-01-04 19:51:39 -0300

    test(in-memory-storage-service-impl): add the in memory storage service impl tests

---
+++ infrastructure/src/test/java/com/kaua/ecommerce/infrastructure/service/local/InMemoryStorageServiceImplTest.java
@@ -0,0 +1,62 @@
+package com.kaua.ecommerce.infrastructure.service.local;
+
+import com.kaua.ecommerce.domain.utils.IdUtils;
+import com.kaua.ecommerce.domain.utils.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("unitTest")
+public class InMemoryStorageServiceImplTest {
+
+    private InMemoryStorageServiceImpl target = new InMemoryStorageServiceImpl();
+
+    @BeforeEach
+    void cleanUp() {
+        this.target.clear();
+    }
+
+    @Test
+    void givenValidResource_whenCallStore_thenShouldUploadFile() {
+        // given
+        final var aFileName = "avatar.png";
+        final var aResource = Resource.with(
+                "content".getBytes(),
+                "image/png",
+                aFileName
+        );
+        final var aKey = IdUtils.generate() + "-" + "COVER" + "-" +
+                IdUtils.generate().replace("-", "")
+                + "-" + aFileName;
+
+        // when
+        this.target.store(aKey, aResource);
+
+        // then
+        Assertions.assertEquals(1, this.target.storage().size());
+    }
+
+    @Test
+    void givenValidLocation_whenCallDelete_thenShouldDeleteFile() {
+        // given
+        final var aFileName = "avatar.png";
+        final var aResource = Resource.with(
+                "content".getBytes(),
+                "image/png",
+                aFileName
+        );
+        final var aKey = IdUtils.generate() + "-" + "COVER" + "-" +
+                IdUtils.generate().replace("-", "")
+                + "-" + aFileName;
+
+        // when
+        this.target.store(aKey, aResource);
+
+        // when
+        this.target.delete(aKey);
+
+        // then
+        Assertions.assertEquals(0, this.target.storage().size());
+    }
+}

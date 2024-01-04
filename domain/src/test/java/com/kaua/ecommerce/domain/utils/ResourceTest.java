ec6b082fd56536ac7bc33b9d79df19310aebceaa
Kauã Pereira <zkauacodes@hotmail.com>
2024-01-04 16:40:20 -0300

    test(resource): refactor tests after remove inputstream

--- domain/src/test/java/com/kaua/ecommerce/domain/utils/ResourceTest.java
+++ domain/src/test/java/com/kaua/ecommerce/domain/utils/ResourceTest.java
@@ -3,57 +3,71 @@ package com.kaua.ecommerce.domain.utils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.InputStream;
-
 public class ResourceTest {
 
     @Test
     void givenAValidParams_whenCallWithResource_shouldInstantiate() {
-        final var aInputStream = InputStream.nullInputStream();
+        final var aContent = "content".getBytes();
         final var aContentType = "image/png";
         final var aFileName = "image.png";
 
-        final var actualResource = Resource.with(aInputStream, aContentType, aFileName);
+        final var actualResource = Resource.with(aContent, aContentType, aFileName);
 
         Assertions.assertNotNull(actualResource);
-        Assertions.assertEquals(aInputStream, actualResource.inputStream());
+        Assertions.assertEquals(aContent, actualResource.content());
         Assertions.assertEquals(aContentType, actualResource.contentType());
         Assertions.assertEquals(aFileName, actualResource.fileName());
     }
 
     @Test
-    void givenAnInvalidNullInputStream_whenCallWithResource_shouldThrowException() {
-        final InputStream aInputStream = null;
+    void givenAValidParamsButFilenameLengthMoreThan20_whenCallWithResource_shouldInstantiate() {
+        final var aContent = "content".getBytes();
+        final var aContentType = "image/png";
+        final var aFilenameWithoutExtension = RandomStringUtils.generateValue(64);
+        final var aFileName = aFilenameWithoutExtension + ".png";
+        final var aFilenameAfterTruncate = aFilenameWithoutExtension.substring(0, 20) + ".png";
+
+        final var actualResource = Resource.with(aContent, aContentType, aFileName);
+
+        Assertions.assertNotNull(actualResource);
+        Assertions.assertEquals(aContent, actualResource.content());
+        Assertions.assertEquals(aContentType, actualResource.contentType());
+        Assertions.assertEquals(aFilenameAfterTruncate, actualResource.fileName());
+    }
+
+    @Test
+    void givenAnInvalidNullContent_whenCallWithResource_shouldThrowException() {
+        final byte[] aContent = null;
         final var aContentType = "image/png";
         final var aFileName = "image.png";
 
         Assertions.assertThrows(
                 NullPointerException.class,
-                () -> Resource.with(aInputStream, aContentType, aFileName)
+                () -> Resource.with(aContent, aContentType, aFileName)
         );
     }
 
     @Test
     void givenAnInvalidNullContentType_whenCallWithResource_shouldThrowException() {
-        final var aInputStream = InputStream.nullInputStream();
+        final var aContent = "content".getBytes();
         final String aContentType = null;
         final var aFileName = "image.png";
 
         Assertions.assertThrows(
                 NullPointerException.class,
-                () -> Resource.with(aInputStream, aContentType, aFileName)
+                () -> Resource.with(aContent, aContentType, aFileName)
         );
     }
 
     @Test
     void givenAnInvalidNullFileName_whenCallWithResource_shouldThrowException() {
-        final var aInputStream = InputStream.nullInputStream();
+        final var aContent = "content".getBytes();
         final var aContentType = "image/png";
         final String aFileName = null;
 
         Assertions.assertThrows(
                 NullPointerException.class,
-                () -> Resource.with(aInputStream, aContentType, aFileName)
+                () -> Resource.with(aContent, aContentType, aFileName)
         );
     }
 }

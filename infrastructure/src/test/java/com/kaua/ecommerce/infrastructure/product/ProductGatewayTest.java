d66174b0119c86d1bc7ec66cadc6a1d053e20bab
Kauã Pereira <zkauacodes@hotmail.com>
2024-01-04 19:54:07 -0300

    test(product-gateway): add the findColorByName, findById and update methods tests

--- infrastructure/src/test/java/com/kaua/ecommerce/infrastructure/product/ProductGatewayTest.java
+++ infrastructure/src/test/java/com/kaua/ecommerce/infrastructure/product/ProductGatewayTest.java
@@ -1,15 +1,18 @@
 package com.kaua.ecommerce.infrastructure.product;
 
+import com.kaua.ecommerce.domain.Fixture;
 import com.kaua.ecommerce.domain.category.CategoryID;
-import com.kaua.ecommerce.domain.product.Product;
-import com.kaua.ecommerce.domain.product.ProductAttributes;
-import com.kaua.ecommerce.domain.product.ProductColor;
-import com.kaua.ecommerce.domain.product.ProductSize;
+import com.kaua.ecommerce.domain.product.*;
 import com.kaua.ecommerce.infrastructure.DatabaseGatewayTest;
+import com.kaua.ecommerce.infrastructure.product.persistence.ProductColorJpaEntity;
+import com.kaua.ecommerce.infrastructure.product.persistence.ProductColorJpaRepository;
+import com.kaua.ecommerce.infrastructure.product.persistence.ProductJpaEntity;
 import com.kaua.ecommerce.infrastructure.product.persistence.ProductJpaRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
 
 @DatabaseGatewayTest
 public class ProductGatewayTest {
@@ -20,11 +23,14 @@ public class ProductGatewayTest {
     @Autowired
     private ProductJpaRepository productRepository;
 
+    @Autowired
+    private ProductColorJpaRepository productColorRepository;
+
     @Test
     void givenAValidProduct_whenCallCreate_shouldPersistProduct() {
         final var aName = "Product Name";
         final var aDescription = "Product Description";
-        final var aPrice = 10.0;
+        final var aPrice = BigDecimal.valueOf(10.0);
         final var aQuantity = 10;
         final var aCategoryId = CategoryID.unique();
         final var aColor = "Red";
@@ -61,10 +67,100 @@ public class ProductGatewayTest {
         Assertions.assertEquals(aProduct.getPrice(), aPersistedProduct.getPrice());
         Assertions.assertEquals(aProduct.getQuantity(), aPersistedProduct.getQuantity());
         Assertions.assertEquals(aProduct.getCategoryId().getValue(), aPersistedProduct.getCategoryId());
+        Assertions.assertEquals(0, aPersistedProduct.getImages().size());
         Assertions.assertNotNull(aPersistedProduct.getAttributes().stream().findFirst().get().getId());
         Assertions.assertNotNull(aPersistedProduct.getAttributes().stream().findFirst().get().getProduct());
         Assertions.assertEquals(aColor, aPersistedProduct.getAttributes().stream().findFirst().get().getColor().getColor());
         Assertions.assertEquals(aSize, aPersistedProduct.getAttributes().stream().findFirst().get().getSize().getSize());
+        Assertions.assertEquals(aProduct.getCreatedAt(), aPersistedProduct.getCreatedAt());
+        Assertions.assertEquals(aProduct.getUpdatedAt(), aPersistedProduct.getUpdatedAt());
+    }
+
+    @Test
+    void givenAValidProductId_whenCallFindById_shouldReturnProduct() {
+        final var aProduct = Fixture.Products.tshirt();
+        final var aProductImage = Fixture.Products.imageCoverType();
+        aProduct.addImage(aProductImage);
+        this.productRepository.save(ProductJpaEntity.toEntity(aProduct));
+
+        Assertions.assertEquals(1, this.productRepository.count());
+
+        final var aPersistedProduct = this.productGateway.findById(aProduct.getId().getValue()).get();
+
+        Assertions.assertEquals(aProduct.getId().getValue(), aPersistedProduct.getId().getValue());
+        Assertions.assertEquals(aProduct.getName(), aPersistedProduct.getName());
+        Assertions.assertEquals(aProduct.getDescription(), aPersistedProduct.getDescription());
+        Assertions.assertEquals(aProduct.getPrice(), aPersistedProduct.getPrice());
+        Assertions.assertEquals(aProduct.getQuantity(), aPersistedProduct.getQuantity());
+        Assertions.assertEquals(aProduct.getCategoryId().getValue(), aPersistedProduct.getCategoryId().getValue());
+        Assertions.assertEquals(1, aPersistedProduct.getImages().size());
+        Assertions.assertEquals(aProduct.getAttributes().stream().findFirst().get().sku(), aPersistedProduct.getAttributes().stream().findFirst().get().sku());
+        Assertions.assertEquals(aProduct.getAttributes().stream().findFirst().get().color().color(), aPersistedProduct.getAttributes().stream().findFirst().get().color().color());
+        Assertions.assertEquals(aProduct.getAttributes().stream().findFirst().get().size().size(), aPersistedProduct.getAttributes().stream().findFirst().get().size().size());
+        Assertions.assertEquals(aProduct.getCreatedAt(), aPersistedProduct.getCreatedAt());
+        Assertions.assertEquals(aProduct.getUpdatedAt(), aPersistedProduct.getUpdatedAt());
+    }
+
+    @Test
+    void givenAnInvalidProductId_whenCallFindById_shouldReturnEmpty() {
+        final var aProductId = ProductID.unique();
+
+        Assertions.assertEquals(0, this.productRepository.count());
+
+        final var aProduct = this.productGateway.findById(aProductId.getValue());
+
+        Assertions.assertTrue(aProduct.isEmpty());
+    }
+
+    @Test
+    void givenAValidColorName_whenCallFindColorByName_shouldReturnProductColor() {
+        final var aProductColor = ProductColor.with("Red");
+        this.productColorRepository.save(ProductColorJpaEntity.toEntity(aProductColor));
+
+        Assertions.assertEquals(1, this.productColorRepository.count());
+
+        final var aPersistedProductColor = this.productGateway.findColorByName(aProductColor.color()).get();
+
+        Assertions.assertEquals(aProductColor.id(), aPersistedProductColor.id());
+        Assertions.assertEquals(aProductColor.color(), aPersistedProductColor.color());
+    }
+
+    @Test
+    void givenAnInvalidColorName_whenCallFindColorByName_shouldReturnEmpty() {
+        final var aProductColorName = "blue";
+
+        Assertions.assertEquals(0, this.productColorRepository.count());
+
+        final var aProduct = this.productGateway.findColorByName(aProductColorName);
+
+        Assertions.assertTrue(aProduct.isEmpty());
+    }
+
+    @Test
+    void givenAValidProductWithImages_whenCallUpdate_shouldUpdateProduct() {
+        final var aProduct = Fixture.Products.tshirt();
+        final var aProductImage = Fixture.Products.imageCoverType();
+
+        this.productRepository.save(ProductJpaEntity.toEntity(aProduct));
+
+        Assertions.assertEquals(1, this.productRepository.count());
+
+        aProduct.addImage(aProductImage);
+
+        final var updatedProduct = this.productGateway.update(aProduct);
+
+        Assertions.assertEquals(1, this.productRepository.count());
+
+        final var aPersistedProduct = this.productRepository.findById(updatedProduct.getId().getValue()).get();
+
+        Assertions.assertEquals(aProduct.getId().getValue(), aPersistedProduct.getId());
+        Assertions.assertEquals(aProduct.getName(), aPersistedProduct.getName());
+        Assertions.assertEquals(aProduct.getDescription(), aPersistedProduct.getDescription());
+        Assertions.assertEquals(aProduct.getPrice(), aPersistedProduct.getPrice());
+        Assertions.assertEquals(aProduct.getQuantity(), aPersistedProduct.getQuantity());
+        Assertions.assertEquals(aProduct.getCategoryId().getValue(), aPersistedProduct.getCategoryId());
+        Assertions.assertEquals(1, aPersistedProduct.getImages().size());
+        Assertions.assertNotNull(aPersistedProduct.getAttributes().stream().findFirst().get().getSku());
         Assertions.assertEquals(aProduct.getCreatedAt(), aPersistedProduct.getCreatedAt());
         Assertions.assertEquals(aProduct.getUpdatedAt(), aPersistedProduct.getUpdatedAt());
     }

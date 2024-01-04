8c0455c37d16847fbea75b40850401ce8041a595
Kauã Pereira <zkauacodes@hotmail.com>
2024-01-04 16:40:48 -0300

    test(product-image): fix tests after add url param

--- domain/src/test/java/com/kaua/ecommerce/domain/product/ProductImageTest.java
+++ domain/src/test/java/com/kaua/ecommerce/domain/product/ProductImageTest.java
@@ -7,25 +7,25 @@ public class ProductImageTest {
 
     @Test
     void givenValidValues_whenCallNewProductImage_shouldBeInstantiateProductImage() {
-        final var aCheckSum = "abc";
         final var aName = "product.jpg";
         final var aLocation = "/images/product.jpg";
+        final var aUrl = "http://localhost:8080/images/product.jpg";
 
-        final var aProductImage = ProductImage.with(aCheckSum, aName, aLocation);
+        final var aProductImage = ProductImage.with(aName, aLocation, aUrl);
 
         Assertions.assertNotNull(aProductImage.id());
-        Assertions.assertEquals(aCheckSum, aProductImage.checksum());
         Assertions.assertEquals(aName, aProductImage.name());
         Assertions.assertEquals(aLocation, aProductImage.location());
+        Assertions.assertEquals(aUrl, aProductImage.url());
     }
 
     @Test
     void givenTwoProductImagesWithSameValues_whenCallEquals_shouldBeReturnTrue() {
-        final var aCheckSum = "abc";
         final var aLocation = "/images/product.jpg";
+        final var aUrl = "http://localhost:8080/images/product.jpg";
 
-        final var aProductImage = ProductImage.with(aCheckSum, "RANDOM", aLocation);
-        final var anotherProductImage = ProductImage.with(aCheckSum, "SIMPLE", aLocation);
+        final var aProductImage = ProductImage.with("1", "RANDOM", aLocation, aUrl);
+        final var anotherProductImage = ProductImage.with("1", "SIMPLE", aLocation, aUrl);
 
         Assertions.assertEquals(aProductImage, anotherProductImage);
         Assertions.assertNotSame(aProductImage, anotherProductImage);
@@ -34,14 +34,13 @@ public class ProductImageTest {
     @Test
     void givenAValidValuesWithId_whenCallWith_shouldBeInstantiateProductImage() {
         final var aId = "abc";
-        final var aCheckSum = "abc";
         final var aName = "product.jpg";
         final var aLocation = "/images/product.jpg";
+        final var aUrl = "http://localhost:8080/images/product.jpg";
 
-        final var aProductImage = ProductImage.with(aId, aCheckSum, aName, aLocation);
+        final var aProductImage = ProductImage.with(aId, aName, aLocation, aUrl);
 
         Assertions.assertEquals(aId, aProductImage.id());
-        Assertions.assertEquals(aCheckSum, aProductImage.checksum());
         Assertions.assertEquals(aName, aProductImage.name());
         Assertions.assertEquals(aLocation, aProductImage.location());
     }
@@ -56,12 +55,15 @@ public class ProductImageTest {
 
         Assertions.assertThrows(NullPointerException.class,
                 () -> ProductImage.with("abc", "Random", null));
+
+        Assertions.assertThrows(NullPointerException.class,
+                () -> ProductImage.with("abc", "Random", null));
     }
 
     @Test
     void testProductImageEqualsAndHashCode() {
-        final var aProductImage = ProductImage.with("abc", "Random", "/images");
-        final var anotherProductImage = ProductImage.with("abc", "Random", "/images");
+        final var aProductImage = ProductImage.with("abc", "Random", "/images", "http://localhost:8080/images");
+        final var anotherProductImage = ProductImage.with("abc", "Random", "/images", "http://localhost:8080/images");
 
         Assertions.assertTrue(aProductImage.equals(anotherProductImage));
         Assertions.assertTrue(aProductImage.equals(aProductImage));
@@ -71,9 +73,9 @@ public class ProductImageTest {
     }
 
     @Test
-    void testProductImageCheckSumNotEquals() {
-        final var aProductImage = ProductImage.with("abc", "Random", "/images");
-        final var anotherProductImage = ProductImage.with("def", "Random", "/images");
+    void testProductImageIdNotEquals() {
+        final var aProductImage = ProductImage.with("abc", "Random", "/images", "http://localhost:8080/images");
+        final var anotherProductImage = ProductImage.with("def", "Random", "/images", "http://localhost:8080/images");
 
         Assertions.assertFalse(aProductImage.equals(anotherProductImage));
         Assertions.assertTrue(aProductImage.equals(aProductImage));
@@ -84,8 +86,8 @@ public class ProductImageTest {
 
     @Test
     void testProductImageLocationNotEquals() {
-        final var aProductImage = ProductImage.with("abc", "Random", "/images");
-        final var anotherProductImage = ProductImage.with("abc", "Random", "/downloads");
+        final var aProductImage = ProductImage.with("abc", "Random", "/images", "http://localhost:8080/images");
+        final var anotherProductImage = ProductImage.with("abc", "Random", "/downloads", "http://localhost:8080/images");
 
         Assertions.assertFalse(aProductImage.equals(anotherProductImage));
         Assertions.assertTrue(aProductImage.equals(aProductImage));

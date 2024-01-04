46699b3ec515b2f62696eee411020d04abcf3182
Kauã Pereira <zkauacodes@hotmail.com>
2024-01-04 16:36:12 -0300

    refactor(resource): change inputstream to byte array and add filename length check

--- domain/src/main/java/com/kaua/ecommerce/domain/utils/Resource.java
+++ domain/src/main/java/com/kaua/ecommerce/domain/utils/Resource.java
@@ -2,27 +2,31 @@ package com.kaua.ecommerce.domain.utils;
 
 import com.kaua.ecommerce.domain.ValueObject;
 
-import java.io.InputStream;
 import java.util.Objects;
 
 public class Resource extends ValueObject {
 
-    private final InputStream inputStream;
+    private final byte[] content;
     private final String contentType;
     private final String fileName;
 
-    private Resource(final InputStream inputStream, final String contentType, final String fileName) {
-        this.inputStream = Objects.requireNonNull(inputStream);
+    private Resource(final byte[] content, final String contentType, final String fileName) {
+        this.content = Objects.requireNonNull(content);
         this.contentType = Objects.requireNonNull(contentType);
         this.fileName = Objects.requireNonNull(fileName);
     }
 
-    public static Resource with(final InputStream inputStream, final String contentType, final String fileName) {
-        return new Resource(inputStream, contentType, fileName);
+    public static Resource with(final byte[] content, final String contentType, final String fileName) {
+        final var aFilenameParts = fileName.split("\\.");
+        if (aFilenameParts[0].length() > 20) {
+            aFilenameParts[0] = aFilenameParts[0].substring(0, 20);
+        }
+        final var aFilename = aFilenameParts[0] + "." + aFilenameParts[1];
+        return new Resource(content, contentType, aFilename);
     }
 
-    public InputStream inputStream() {
-        return inputStream;
+    public byte[] content() {
+        return content;
     }
 
     public String contentType() {

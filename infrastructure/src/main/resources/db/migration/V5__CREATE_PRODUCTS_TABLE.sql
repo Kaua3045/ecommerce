ab9dfe1458181083c565d3deb5dd288369fdcdb4
Kauã Pereira <zkauacodes@hotmail.com>
2024-01-04 19:48:10 -0300

    feat(migration): add the products tables migration

--- infrastructure/src/main/resources/db/migration/V5__CREATE_PRODUCTS_TABLE.sql
+++ infrastructure/src/main/resources/db/migration/V5__CREATE_PRODUCTS_TABLE.sql
@@ -2,7 +2,7 @@ CREATE TABLE products (
     id VARCHAR(36) PRIMARY KEY NOT NULL,
     name VARCHAR(255) NOT NULL,
     description VARCHAR(255),
-    price double NOT NULL,
+    price NUMERIC(19, 2) NOT NULL,
     quantity integer NOT NULL,
     category_id VARCHAR(36) NOT NULL,
     created_at DATETIME(6) NOT NULL,
@@ -31,4 +31,18 @@ CREATE TABLE products_attributes (
     FOREIGN KEY (product_id) REFERENCES products(id),
     FOREIGN KEY (color_id) REFERENCES products_colors(id),
     FOREIGN KEY (size_id) REFERENCES products_sizes(id)
+);
+
+CREATE TABLE products_images (
+    id VARCHAR(36) PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE products_images_relations (
+    product_id VARCHAR(36) NOT NULL,
+    image_id VARCHAR(36) NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(id),
+    FOREIGN KEY (image_id) REFERENCES products_images(id)
 );

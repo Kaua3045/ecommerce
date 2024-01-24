97e2267de444ba2fffc1517bd268acccfadce68e
Kauã Pereira <zkauacodes@hotmail.com>
2024-01-24 19:25:37 -0300

    feat(upload-product-image-command): add the list product image resources

--- application/src/main/java/com/kaua/ecommerce/application/usecases/product/media/upload/UploadProductImageCommand.java
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/product/media/upload/UploadProductImageCommand.java
@@ -2,15 +2,17 @@ package com.kaua.ecommerce.application.usecases.product.media.upload;
 
 import com.kaua.ecommerce.domain.product.ProductImageResource;
 
+import java.util.List;
+
 public record UploadProductImageCommand(
         String productId,
-        ProductImageResource productImageResource
+        List<ProductImageResource> productImagesResources
 ) {
 
     public static UploadProductImageCommand with(
             final String aProductId,
-            final ProductImageResource aProductImageResource
+            final List<ProductImageResource> aProductImagesResources
     ) {
-        return new UploadProductImageCommand(aProductId, aProductImageResource);
+        return new UploadProductImageCommand(aProductId, aProductImagesResources);
     }
 }

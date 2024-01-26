ee7bc11b9ab099e783dded074e97512303c7e496
Kauã Pereira <zkauacodes@hotmail.com>
2024-01-26 08:59:41 -0300

    feat(product-attributes-id): add the color id and size id to use in equals

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/product/persistence/ProductAttributesId.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/product/persistence/ProductAttributesId.java
@@ -12,31 +12,55 @@ public class ProductAttributesId implements Serializable {
     @Column(name = "product_id", nullable = false)
     private String productId;
 
+    @Column(name = "color_id", nullable = false)
+    private String colorId;
+
+    @Column(name = "size_id", nullable = false)
+    private String sizeId;
+
     public ProductAttributesId() {
     }
 
-    private ProductAttributesId(final String productId) {
+    private ProductAttributesId(
+            final String productId,
+            final String colorId,
+            final String sizeId
+    ) {
         this.productId = productId;
+        this.colorId = colorId;
+        this.sizeId = sizeId;
     }
 
-    public static ProductAttributesId from(final String productId) {
-        return new ProductAttributesId(productId);
+    public static ProductAttributesId from(
+            final String productId,
+            final String colorId,
+            final String sizeId
+    ) {
+        return new ProductAttributesId(productId, colorId, sizeId);
     }
 
     @Override
-    public boolean equals(final Object o) {
+    public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        final ProductAttributesId that = (ProductAttributesId) o;
-        return Objects.equals(getProductId(), that.getProductId());
+        ProductAttributesId that = (ProductAttributesId) o;
+        return Objects.equals(getProductId(), that.getProductId()) && Objects.equals(getColorId(), that.getColorId()) && Objects.equals(getSizeId(), that.getSizeId());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getProductId());
+        return Objects.hash(getProductId(), getColorId(), getSizeId());
     }
 
     public String getProductId() {
         return productId;
+    }
+
+    public String getColorId() {
+        return colorId;
+    }
+
+    public String getSizeId() {
+        return sizeId;
     }
 }

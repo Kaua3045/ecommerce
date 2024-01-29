d2e98bfbc3d4434ad39c3890761e7e2805e9b66f
Kauã Pereira <zkauacodes@hotmail.com>
2024-01-29 15:01:32 -0300

    feat(delete-product-usecase): add the delete product usecase abstract class

---
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/product/delete/DeleteProductUseCase.java
@@ -0,0 +1,6 @@
+package com.kaua.ecommerce.application.usecases.product.delete;
+
+import com.kaua.ecommerce.application.UnitUseCase;
+
+public abstract class DeleteProductUseCase extends UnitUseCase<String> {
+}

ac1a28a07b5840103dcbb5b724408373a5770838
Kauã Pereira <zkauacodes@hotmail.com>
2024-01-29 15:00:46 -0300

    feat(media-resource-gateway): add the clearImages method

--- application/src/main/java/com/kaua/ecommerce/application/gateways/MediaResourceGateway.java
+++ application/src/main/java/com/kaua/ecommerce/application/gateways/MediaResourceGateway.java
@@ -4,9 +4,13 @@ import com.kaua.ecommerce.domain.product.ProductID;
 import com.kaua.ecommerce.domain.product.ProductImage;
 import com.kaua.ecommerce.domain.product.ProductImageResource;
 
+import java.util.Set;
+
 public interface MediaResourceGateway {
 
     ProductImage storeImage(ProductID aProductID, ProductImageResource aResource);
 
     void clearImage(ProductImage aImage);
+
+    void clearImages(Set<ProductImage> aImages);
 }

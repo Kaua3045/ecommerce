90b1a15c9930bd437535f4898ac9c19c5c092bc9
Kauã Pereira <zkauacodes@hotmail.com>
2024-01-30 14:49:21 -0300

    feat(delete-product-usecase): remove media resource gateway and update product status to deleted and after update product in database

--- application/src/main/java/com/kaua/ecommerce/application/usecases/product/delete/DefaultDeleteProductUseCase.java
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/product/delete/DefaultDeleteProductUseCase.java
@@ -1,32 +1,25 @@
 package com.kaua.ecommerce.application.usecases.product.delete;
 
-import com.kaua.ecommerce.application.gateways.MediaResourceGateway;
 import com.kaua.ecommerce.application.gateways.ProductGateway;
+import com.kaua.ecommerce.domain.product.ProductStatus;
+import com.kaua.ecommerce.domain.product.events.ProductDeletedEvent;
 
 import java.util.Objects;
 
 public class DefaultDeleteProductUseCase extends DeleteProductUseCase {
 
     private final ProductGateway productGateway;
-    private final MediaResourceGateway mediaResourceGateway;
 
-    public DefaultDeleteProductUseCase(
-            final ProductGateway productGateway,
-            final MediaResourceGateway mediaResourceGateway
-    ) {
+    public DefaultDeleteProductUseCase(final ProductGateway productGateway) {
         this.productGateway = Objects.requireNonNull(productGateway);
-        this.mediaResourceGateway = Objects.requireNonNull(mediaResourceGateway);
     }
 
-    // TODO: refactor this method to publish an event to delete the product and its images from the storage
-    // before persisting the delete event inactivate product in database
     @Override
     public void execute(String aId) {
-        this.productGateway.findById(aId).ifPresent(product -> {
-            this.productGateway.delete(aId);
-            product.getBannerImage().ifPresent(this.mediaResourceGateway::clearImage);
-            if (!product.getImages().isEmpty())
-                this.mediaResourceGateway.clearImages(product.getImages());
+        this.productGateway.findById(aId).ifPresent(aProduct -> {
+            aProduct.updateStatus(ProductStatus.DELETED);
+            aProduct.registerEvent(ProductDeletedEvent.from(aProduct));
+            this.productGateway.update(aProduct);
         });
     }
 }

1b72c1cc41e2cee883b0c4354e7d8bebff359a9f
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-01 08:40:26 -0300

    refactor(product-is-deleted-exception): refactor exception message

--- application/src/main/java/com/kaua/ecommerce/application/exceptions/ProductIsDeletedException.java
+++ application/src/main/java/com/kaua/ecommerce/application/exceptions/ProductIsDeletedException.java
@@ -1,12 +1,20 @@
 package com.kaua.ecommerce.application.exceptions;
 
 import com.kaua.ecommerce.domain.exceptions.DomainException;
+import com.kaua.ecommerce.domain.product.ProductID;
+import com.kaua.ecommerce.domain.validation.Error;
 
 import java.util.Collections;
+import java.util.List;
 
 public class ProductIsDeletedException extends DomainException {
 
-    public ProductIsDeletedException() {
-        super("Product is deleted", Collections.emptyList());
+    protected ProductIsDeletedException(String aMessage, List<Error> aErrors) {
+        super(aMessage, aErrors);
+    }
+
+    public static ProductIsDeletedException with(ProductID id) {
+        final var aError = "Product with id %s is deleted".formatted(id.getValue());
+        return new ProductIsDeletedException(aError, Collections.emptyList());
     }
 }

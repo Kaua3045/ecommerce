a3dfe3e6b19569aaa9b76967d5f096e88574ab7e
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-18 16:22:47 -0300

    feat(get-product-by-id-output): add the get product by id output record class

---
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/product/retrieve/get/GetProductByIdOutput.java
@@ -0,0 +1,43 @@
+package com.kaua.ecommerce.application.usecases.product.retrieve.get;
+
+import com.kaua.ecommerce.domain.product.Product;
+import com.kaua.ecommerce.domain.utils.CollectionUtils;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.Set;
+
+public record GetProductByIdOutput(
+        String productId,
+        String name,
+        String description,
+        BigDecimal price,
+        int quantity,
+        String categoryId,
+        GetProductByIdOutputImages bannerImage,
+        Set<GetProductByIdOutputImages> galleryImages,
+        Set<GetProductByIdOutputAttributes> attributes,
+        String status,
+        Instant createdAt,
+        Instant updatedAt,
+        long version
+) {
+
+    public static GetProductByIdOutput from(final Product aProduct) {
+        return new GetProductByIdOutput(
+                aProduct.getId().getValue(),
+                aProduct.getName(),
+                aProduct.getDescription(),
+                aProduct.getPrice(),
+                aProduct.getQuantity(),
+                aProduct.getCategoryId().getValue(),
+                aProduct.getBannerImage().map(GetProductByIdOutputImages::from).orElse(null),
+                CollectionUtils.mapTo(aProduct.getImages(), GetProductByIdOutputImages::from),
+                CollectionUtils.mapTo(aProduct.getAttributes(), GetProductByIdOutputAttributes::from),
+                aProduct.getStatus().name(),
+                aProduct.getCreatedAt(),
+                aProduct.getUpdatedAt(),
+                aProduct.getVersion()
+        );
+    }
+}

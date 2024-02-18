8ce8994d67d64acc7a9eac368f7052004586a372
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-18 16:23:18 -0300

    feat(get-product-by-id-usecase): add the get product by id usecase abstract

---
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/product/retrieve/get/GetProductByIdUseCase.java
@@ -0,0 +1,6 @@
+package com.kaua.ecommerce.application.usecases.product.retrieve.get;
+
+import com.kaua.ecommerce.application.UseCase;
+
+public abstract class GetProductByIdUseCase extends UseCase<GetProductByIdOutput, String> {
+}

39a657530bb2a4b37f6bd4198647cabed35b620b
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-19 14:38:03 -0300

    refactor(product-cannot-have-more-attributes-and-images): before is cant after is cannot

--- domain/src/main/java/com/kaua/ecommerce/domain/exceptions/ProductCannotHaveMoreImagesException.java
+++ domain/src/main/java/com/kaua/ecommerce/domain/exceptions/ProductCannotHaveMoreImagesException.java
@@ -2,9 +2,9 @@ package com.kaua.ecommerce.domain.exceptions;
 
 import java.util.Collections;
 
-public class ProductNotHaveMoreImagesException extends DomainException {
+public class ProductCannotHaveMoreImagesException extends DomainException {
 
-    public ProductNotHaveMoreImagesException() {
+    public ProductCannotHaveMoreImagesException() {
         super("Product can't have more than 20 images", Collections.emptyList());
     }
 }

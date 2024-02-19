0a4cc7c5f5bdfcb7765206e9ddf5564a4e97172a
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-19 12:27:11 -0300

    feat(add-product-attributes-param-input): add the add product attributes param input with json property annotations

---
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/product/models/AddProductAttributesParamInput.java
@@ -0,0 +1,13 @@
+package com.kaua.ecommerce.infrastructure.product.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record AddProductAttributesParamInput(
+        @JsonProperty("color_name") String colorName,
+        @JsonProperty("size_name") String sizeName,
+        @JsonProperty("weight") double weight,
+        @JsonProperty("height") double height,
+        @JsonProperty("width") double width,
+        @JsonProperty("depth") double depth
+) {
+}

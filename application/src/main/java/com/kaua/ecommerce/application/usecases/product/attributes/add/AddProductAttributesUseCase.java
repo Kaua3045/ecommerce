f03e6102b86b7b64c74d7fbbd79127ed9db4b232
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-19 12:24:39 -0300

    feat(add-product-attributes-usecase): add the add product attributes usecase abstract class

---
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/product/attributes/add/AddProductAttributesUseCase.java
@@ -0,0 +1,7 @@
+package com.kaua.ecommerce.application.usecases.product.attributes.add;
+
+import com.kaua.ecommerce.application.UseCase;
+
+public abstract class AddProductAttributesUseCase extends
+        UseCase<AddProductAttributesOutput, AddProductAttributesCommand> {
+}

5b2e6b5346547f3c4241b96494ed1273eff46994
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-19 08:19:58 -0300

    feat(product-elasticsearch-gateway): add the findAll method implementation

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/product/ProductElasticsearchGateway.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/product/ProductElasticsearchGateway.java
@@ -6,8 +6,16 @@ import com.kaua.ecommerce.domain.pagination.SearchQuery;
 import com.kaua.ecommerce.domain.product.Product;
 import com.kaua.ecommerce.infrastructure.product.persistence.elasticsearch.ProductElasticsearchEntity;
 import com.kaua.ecommerce.infrastructure.product.persistence.elasticsearch.ProductElasticsearchRepository;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchOperations;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
+import org.springframework.data.elasticsearch.core.query.Query;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,10 +27,19 @@ public class ProductElasticsearchGateway implements SearchGateway<Product> {
 
     private static final Logger log = LoggerFactory.getLogger(ProductElasticsearchGateway.class);
 
-    private final ProductElasticsearchRepository productElasticsearchRepository;
+    private static final String NAME_PROP = "name";
+    private static final String CATEGORY_ID_PROP = "category_id";
+    private static final String KEYWORD = ".keyword";
 
-    public ProductElasticsearchGateway(final ProductElasticsearchRepository productElasticsearchRepository) {
+    private final ProductElasticsearchRepository productElasticsearchRepository;
+    private final SearchOperations searchOperations;
+
+    public ProductElasticsearchGateway(
+            final ProductElasticsearchRepository productElasticsearchRepository,
+            final SearchOperations searchOperations
+    ) {
         this.productElasticsearchRepository = Objects.requireNonNull(productElasticsearchRepository);
+        this.searchOperations = Objects.requireNonNull(searchOperations);
     }
 
     @Transactional
@@ -35,7 +52,35 @@ public class ProductElasticsearchGateway implements SearchGateway<Product> {
 
     @Override
     public Pagination<Product> findAll(SearchQuery aQuery) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        final var aTerms = aQuery.terms();
+        final var aCurrentPage = aQuery.page();
+        final var aPerPage = aQuery.perPage();
+
+        final var aSort = Sort.by(Sort.Direction.fromString(aQuery.direction()), buildSort(aQuery.sort()));
+        final var aPage = PageRequest.of(aCurrentPage, aPerPage, aSort);
+
+        final Query query;
+        if (StringUtils.isNotEmpty(aTerms)) {
+            final var aCriteria = Criteria.where(NAME_PROP).contains(aTerms)
+                    .or(CATEGORY_ID_PROP).is(aTerms)
+                    .or("description").contains(aTerms);
+
+            query = new CriteriaQuery(aCriteria, aPage);
+        } else {
+            query = Query.findAll().setPageable(aPage);
+        }
+
+        final var aResult = this.searchOperations.search(query, ProductElasticsearchEntity.class);
+
+        final var aTotal = aResult.getTotalHits();
+        final var aTotalPages = (int) Math.ceil((double) aTotal / aPerPage);
+
+        final var aProducts = aResult.stream()
+                .map(SearchHit::getContent)
+                .map(ProductElasticsearchEntity::toDomain)
+                .toList();
+
+        return new Pagination<>(aCurrentPage, aPerPage, aTotalPages, aTotal, aProducts);
     }
 
     @Override
@@ -53,5 +98,12 @@ public class ProductElasticsearchGateway implements SearchGateway<Product> {
     public void deleteById(String id) {
         this.productElasticsearchRepository.deleteById(id);
         log.info("deleted product from elasticsearch: {}", id);
+    }
+
+    private String buildSort(final String aSort) {
+        if (aSort.equals(NAME_PROP) || aSort.equals(CATEGORY_ID_PROP)) {
+            return aSort.concat(KEYWORD);
+        }
+        return aSort;
     }
 }

6cd3d60989a0f68be87a810fb747a69c695b757c
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-19 10:31:22 -0300

    feat(remove-product-image-usecase): add the remove product image usecase abstract class

---
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/product/media/remove/RemoveProductImageUseCase.java
@@ -0,0 +1,6 @@
+package com.kaua.ecommerce.application.usecases.product.media.remove;
+
+import com.kaua.ecommerce.application.UnitUseCase;
+
+public abstract class RemoveProductImageUseCase extends UnitUseCase<RemoveProductImageCommand> {
+}

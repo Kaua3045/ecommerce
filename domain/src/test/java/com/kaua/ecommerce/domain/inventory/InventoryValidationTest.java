a19ab54bb8b5c6309fa2b4aed7f30f7a06b15031
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-20 17:20:54 -0300

    test(inventory-validation): add the inventory validation tests

---
+++ domain/src/test/java/com/kaua/ecommerce/domain/inventory/InventoryValidationTest.java
@@ -0,0 +1,99 @@
+package com.kaua.ecommerce.domain.inventory;
+
+import com.kaua.ecommerce.domain.TestValidationHandler;
+import com.kaua.ecommerce.domain.utils.CommonErrorMessage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InventoryValidationTest {
+
+    @Test
+    void givenInvalidBlankProductId_whenCallNewInventory_shouldReturnDomainException() {
+        final var aProductId = " ";
+        final var aSku = "sku";
+        final var aQuantity = 10;
+
+        final var expectedErrorMessage = CommonErrorMessage.nullOrBlank("productId");
+        final var expectedErrorCount = 1;
+
+        final var aInventory = Inventory.newInventory(aProductId, aSku, aQuantity);
+
+        final var aTestValidationHandler = new TestValidationHandler();
+        aInventory.validate(aTestValidationHandler);
+
+        Assertions.assertEquals(expectedErrorCount, aTestValidationHandler.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, aTestValidationHandler.getErrors().get(0).message());
+    }
+
+    @Test
+    void givenInvalidNullProductId_whenCallNewInventory_shouldReturnDomainException() {
+        final String aProductId = null;
+        final var aSku = "sku";
+        final var aQuantity = 10;
+
+        final var expectedErrorMessage = CommonErrorMessage.nullOrBlank("productId");
+        final var expectedErrorCount = 1;
+
+        final var aInventory = Inventory.newInventory(aProductId, aSku, aQuantity);
+
+        final var aTestValidationHandler = new TestValidationHandler();
+        aInventory.validate(aTestValidationHandler);
+
+        Assertions.assertEquals(expectedErrorCount, aTestValidationHandler.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, aTestValidationHandler.getErrors().get(0).message());
+    }
+
+    @Test
+    void givenInvalidBlankSku_whenCallNewInventory_shouldReturnDomainException() {
+        final var aProductId = "1";
+        final var aSku = " ";
+        final var aQuantity = 10;
+
+        final var expectedErrorMessage = CommonErrorMessage.nullOrBlank("sku");
+        final var expectedErrorCount = 1;
+
+        final var aInventory = Inventory.newInventory(aProductId, aSku, aQuantity);
+
+        final var aTestValidationHandler = new TestValidationHandler();
+        aInventory.validate(aTestValidationHandler);
+
+        Assertions.assertEquals(expectedErrorCount, aTestValidationHandler.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, aTestValidationHandler.getErrors().get(0).message());
+    }
+
+    @Test
+    void givenInvalidNullSku_whenCallNewInventory_shouldReturnDomainException() {
+        final var aProductId = "1";
+        final String aSku = null;
+        final var aQuantity = 10;
+
+        final var expectedErrorMessage = CommonErrorMessage.nullOrBlank("sku");
+        final var expectedErrorCount = 1;
+
+        final var aInventory = Inventory.newInventory(aProductId, aSku, aQuantity);
+
+        final var aTestValidationHandler = new TestValidationHandler();
+        aInventory.validate(aTestValidationHandler);
+
+        Assertions.assertEquals(expectedErrorCount, aTestValidationHandler.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, aTestValidationHandler.getErrors().get(0).message());
+    }
+
+    @Test
+    void givenInvalidQuantityLessThanZero_whenCallNewInventory_shouldReturnDomainException() {
+        final var aProductId = "1";
+        final var aSku = "sku";
+        final var aQuantity = -1;
+
+        final var expectedErrorMessage = CommonErrorMessage.greaterThan("quantity", -1);
+        final var expectedErrorCount = 1;
+
+        final var aInventory = Inventory.newInventory(aProductId, aSku, aQuantity);
+
+        final var aTestValidationHandler = new TestValidationHandler();
+        aInventory.validate(aTestValidationHandler);
+
+        Assertions.assertEquals(expectedErrorCount, aTestValidationHandler.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, aTestValidationHandler.getErrors().get(0).message());
+    }
+}

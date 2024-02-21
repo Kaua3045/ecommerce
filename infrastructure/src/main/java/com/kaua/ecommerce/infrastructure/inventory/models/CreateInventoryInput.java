22d602caf839b1ac4207156c1502d6adcd99afcf
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-21 13:38:55 -0300

    feat(create-inventory-input): add the list with sku and quantity

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/inventory/models/CreateInventoryInput.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/inventory/models/CreateInventoryInput.java
@@ -1,10 +1,20 @@
 package com.kaua.ecommerce.infrastructure.inventory.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.kaua.ecommerce.application.usecases.inventory.create.commands.CreateInventoryCommandParams;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public record CreateInventoryInput(
         @JsonProperty("product_id") String productId,
-        @JsonProperty("sku") String sku,
-        @JsonProperty("quantity") int quantity
+        @JsonProperty("attributes") List<CreateInventoryInputParams> inventoryParams
 ) {
+
+    public Set<CreateInventoryCommandParams> toCommandInventoryParams() {
+        return inventoryParams.stream()
+                .map(it -> CreateInventoryCommandParams.with(it.sku(), it.quantity()))
+                .collect(Collectors.toSet());
+    }
 }

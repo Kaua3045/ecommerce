fe9d523aee615659d3976ea9e5126029cd94ab82
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-21 13:37:41 -0300

    test(create-inventory-usecase): refactor create inventory usecase to create in batch tests

--- application/src/test/java/com/kaua/ecommerce/application/usecases/inventory/create/CreateInventoryUseCaseTest.java
+++ application/src/test/java/com/kaua/ecommerce/application/usecases/inventory/create/CreateInventoryUseCaseTest.java
@@ -2,6 +2,9 @@ package com.kaua.ecommerce.application.usecases.inventory.create;
 
 import com.kaua.ecommerce.application.UseCaseTest;
 import com.kaua.ecommerce.application.gateways.InventoryGateway;
+import com.kaua.ecommerce.application.usecases.inventory.create.commands.CreateInventoryCommand;
+import com.kaua.ecommerce.application.usecases.inventory.create.commands.CreateInventoryCommandParams;
+import com.kaua.ecommerce.domain.exceptions.DomainException;
 import com.kaua.ecommerce.domain.utils.CommonErrorMessage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,7 +12,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.argThat;
@@ -23,58 +29,102 @@ public class CreateInventoryUseCaseTest extends UseCaseTest {
     private DefaultCreateInventoryUseCase createInventoryUseCase;
 
     @Test
-    void givenAValidCommand_whenCallExecute_thenShouldCreateInventory() {
+    void givenAValidCommand_whenCallExecute_thenShouldCreateInventories() {
         final var aProductId = "1";
-        final var aSku = "sku";
-        final var aQuantity = 10;
+        final var aSkuOne = "sku-one";
+        final var aQuantityOne = 10;
+        final var aSkuTwo = "sku-two";
+        final var aQuantityTwo = 20;
+
+        final var aParams = new HashSet<CreateInventoryCommandParams>();
+        aParams.add(CreateInventoryCommandParams.with(aSkuOne, aQuantityOne));
+        aParams.add(CreateInventoryCommandParams.with(aSkuTwo, aQuantityTwo));
 
         final var aCommand = CreateInventoryCommand.with(
                 aProductId,
-                aSku,
-                aQuantity
+                aParams
         );
 
-        Mockito.when(this.inventoryGateway.existsBySku(Mockito.any())).thenReturn(false);
-        Mockito.when(this.inventoryGateway.create(Mockito.any())).then(returnsFirstArg());
+        Mockito.when(this.inventoryGateway.existsBySkus(Mockito.any())).thenReturn(List.of());
+        Mockito.when(this.inventoryGateway.createInBatch(Mockito.any())).then(returnsFirstArg());
 
         final var aOutput = this.createInventoryUseCase.execute(aCommand).getRight();
 
         Assertions.assertNotNull(aOutput);
-        Assertions.assertNotNull(aOutput.inventoryId());
+        Assertions.assertNotNull(aOutput.inventories());
         Assertions.assertNotNull(aOutput.productId());
-        Assertions.assertNotNull(aOutput.sku());
+        Assertions.assertEquals(2, aOutput.inventories().size());
 
-        Mockito.verify(inventoryGateway, Mockito.times(1)).existsBySku(aSku);
-        Mockito.verify(inventoryGateway, Mockito.times(1)).create(argThat(aCmd ->
-                Objects.equals(aCmd.getProductId(), aProductId)
-                        && Objects.equals(aCmd.getSku(), aSku)
-                        && Objects.equals(aCmd.getQuantity(), aQuantity)
-                        && Objects.nonNull(aCmd.getCreatedAt())
-                        && Objects.nonNull(aCmd.getUpdatedAt())));
+        Mockito.verify(inventoryGateway, Mockito.times(1))
+                .existsBySkus(Mockito.any());
+        Mockito.verify(inventoryGateway, Mockito.times(1)).createInBatch(argThat(aCmd ->
+                Objects.equals(2, aCmd.size())));
     }
 
     @Test
-    void givenAnInvalidSkuAlreadyExists_whenCallExecute_shouldReturnDomainException() {
+    void givenAValidCommandButOneSkuIsExists_whenCallExecute_thenShouldCreateInventoriesSizeIsOne() {
         final var aProductId = "1";
-        final var aSku = "sku";
-        final var aQuantity = 10;
+        final var aSkuOne = "sku-one";
+        final var aQuantityOne = 10;
+        final var aSkuTwo = "sku-two";
+        final var aQuantityTwo = 20;
 
-        final var expectedErrorMessage = "'sku' already exists";
+        final var aParams = new HashSet<CreateInventoryCommandParams>();
+        aParams.add(CreateInventoryCommandParams.with(aSkuOne, aQuantityOne));
+        aParams.add(CreateInventoryCommandParams.with(aSkuTwo, aQuantityTwo));
 
         final var aCommand = CreateInventoryCommand.with(
                 aProductId,
-                aSku,
-                aQuantity
+                aParams
         );
 
-        Mockito.when(this.inventoryGateway.existsBySku(Mockito.any())).thenReturn(true);
+        Mockito.when(this.inventoryGateway.existsBySkus(Mockito.any()))
+                .thenReturn(List.of(aSkuTwo));
+        Mockito.when(this.inventoryGateway.createInBatch(Mockito.any())).then(returnsFirstArg());
 
-        final var aOutput = this.createInventoryUseCase.execute(aCommand).getLeft();
+        final var aOutput = this.createInventoryUseCase.execute(aCommand).getRight();
+
+        Assertions.assertNotNull(aOutput);
+        Assertions.assertNotNull(aOutput.inventories());
+        Assertions.assertNotNull(aOutput.productId());
+        Assertions.assertEquals(1, aOutput.inventories().size());
+
+        Mockito.verify(inventoryGateway, Mockito.times(1))
+                .existsBySkus(Mockito.any());
+        Mockito.verify(inventoryGateway, Mockito.times(1)).createInBatch(argThat(aCmd ->
+                Objects.equals(1, aCmd.size())));
+    }
+
+    @Test
+    void givenAnInvalidAllSkuAlreadyExists_whenCallExecute_shouldThrowDomainException() {
+        final var aProductId = "1";
+        final var aSkuOne = "sku-one";
+        final var aQuantityOne = 10;
+        final var aSkuTwo = "sku-two";
+        final var aQuantityTwo = 20;
+
+        final var expectedErrorMessage = "All skus already exists";
+
+        final var aParams = new HashSet<CreateInventoryCommandParams>();
+        aParams.add(CreateInventoryCommandParams.with(aSkuOne, aQuantityOne));
+        aParams.add(CreateInventoryCommandParams.with(aSkuTwo, aQuantityTwo));
+
+        final var aCommand = CreateInventoryCommand.with(
+                aProductId,
+                aParams
+        );
+
+        Mockito.when(this.inventoryGateway.existsBySkus(Mockito.any()))
+                .thenReturn(List.of(aSkuOne, aSkuTwo));
+
+        final var aOutput = Assertions.assertThrows(DomainException.class,
+                () -> this.createInventoryUseCase.execute(aCommand));
 
         Assertions.assertEquals(expectedErrorMessage, aOutput.getErrors().get(0).message());
 
-        Mockito.verify(inventoryGateway, Mockito.times(1)).existsBySku(aSku);
-        Mockito.verify(inventoryGateway, Mockito.times(0)).create(Mockito.any());
+        Mockito.verify(inventoryGateway, Mockito.times(1))
+                .existsBySkus(Mockito.any());
+        Mockito.verify(inventoryGateway, Mockito.times(0)).createInBatch(Mockito.any());
     }
 
     @Test
@@ -85,20 +135,24 @@ public class CreateInventoryUseCaseTest extends UseCaseTest {
 
         final var expectedErrorMessage = CommonErrorMessage.nullOrBlank("productId");
 
+        final var aParams = new HashSet<CreateInventoryCommandParams>();
+        aParams.add(CreateInventoryCommandParams.with(aSku, aQuantity));
+
         final var aCommand = CreateInventoryCommand.with(
                 aProductId,
-                aSku,
-                aQuantity
+                aParams
         );
 
-        Mockito.when(this.inventoryGateway.existsBySku(Mockito.any())).thenReturn(false);
+
+        Mockito.when(this.inventoryGateway.existsBySkus(Mockito.any()))
+                .thenReturn(List.of());
 
         final var aOutput = this.createInventoryUseCase.execute(aCommand).getLeft();
 
         Assertions.assertEquals(expectedErrorMessage, aOutput.getErrors().get(0).message());
 
-        Mockito.verify(inventoryGateway, Mockito.times(1)).existsBySku(aSku);
-        Mockito.verify(inventoryGateway, Mockito.times(0)).create(Mockito.any());
+        Mockito.verify(inventoryGateway, Mockito.times(1)).existsBySkus(List.of(aSku));
+        Mockito.verify(inventoryGateway, Mockito.times(0)).createInBatch(Mockito.any());
     }
 
     @Test
@@ -109,20 +163,24 @@ public class CreateInventoryUseCaseTest extends UseCaseTest {
 
         final var expectedErrorMessage = CommonErrorMessage.nullOrBlank("sku");
 
+        final var aParams = new HashSet<CreateInventoryCommandParams>();
+        aParams.add(CreateInventoryCommandParams.with(aSku, aQuantity));
+
         final var aCommand = CreateInventoryCommand.with(
                 aProductId,
-                aSku,
-                aQuantity
+                aParams
         );
 
-        Mockito.when(this.inventoryGateway.existsBySku(Mockito.any())).thenReturn(false);
+        Mockito.when(this.inventoryGateway.existsBySkus(Mockito.any()))
+                .thenReturn(List.of());
 
         final var aOutput = this.createInventoryUseCase.execute(aCommand).getLeft();
 
         Assertions.assertEquals(expectedErrorMessage, aOutput.getErrors().get(0).message());
 
-        Mockito.verify(inventoryGateway, Mockito.times(1)).existsBySku(aSku);
-        Mockito.verify(inventoryGateway, Mockito.times(0)).create(Mockito.any());
+        Mockito.verify(inventoryGateway, Mockito.times(1))
+                .existsBySkus(List.of(aSku));
+        Mockito.verify(inventoryGateway, Mockito.times(0)).createInBatch(Mockito.any());
     }
 
     @Test
@@ -133,19 +191,64 @@ public class CreateInventoryUseCaseTest extends UseCaseTest {
 
         final var expectedErrorMessage = CommonErrorMessage.greaterThan("quantity", -1);
 
+        final var aParams = new HashSet<CreateInventoryCommandParams>();
+        aParams.add(CreateInventoryCommandParams.with(aSku, aQuantity));
+
         final var aCommand = CreateInventoryCommand.with(
                 aProductId,
-                aSku,
-                aQuantity
+                aParams
         );
 
-        Mockito.when(this.inventoryGateway.existsBySku(Mockito.any())).thenReturn(false);
+        Mockito.when(this.inventoryGateway.existsBySkus(Mockito.any()))
+                .thenReturn(List.of());
 
         final var aOutput = this.createInventoryUseCase.execute(aCommand).getLeft();
 
         Assertions.assertEquals(expectedErrorMessage, aOutput.getErrors().get(0).message());
 
-        Mockito.verify(inventoryGateway, Mockito.times(1)).existsBySku(aSku);
-        Mockito.verify(inventoryGateway, Mockito.times(0)).create(Mockito.any());
+        Mockito.verify(inventoryGateway, Mockito.times(1)).existsBySkus(List.of(aSku));
+        Mockito.verify(inventoryGateway, Mockito.times(0)).createInBatch(Mockito.any());
+    }
+
+    @Test
+    void givenAnInvalidNullParams_whenCallExecute_shouldThrowDomainException() {
+        final var aProductId = "1";
+
+        final var expectedErrorMessage = "Inventory params cannot be null or empty";
+
+        final var aCommand = CreateInventoryCommand.with(
+                aProductId,
+                null
+        );
+
+        final var aOutput = Assertions.assertThrows(DomainException.class,
+                () -> this.createInventoryUseCase.execute(aCommand));
+
+        Assertions.assertEquals(expectedErrorMessage, aOutput.getErrors().get(0).message());
+
+        Mockito.verify(inventoryGateway, Mockito.times(0))
+                .existsBySkus(Mockito.any());
+        Mockito.verify(inventoryGateway, Mockito.times(0)).createInBatch(Mockito.any());
+    }
+
+    @Test
+    void givenAnInvalidEmptyParams_whenCallExecute_shouldThrowDomainException() {
+        final var aProductId = "1";
+
+        final var expectedErrorMessage = "Inventory params cannot be null or empty";
+
+        final var aCommand = CreateInventoryCommand.with(
+                aProductId,
+                Set.of()
+        );
+
+        final var aOutput = Assertions.assertThrows(DomainException.class,
+                () -> this.createInventoryUseCase.execute(aCommand));
+
+        Assertions.assertEquals(expectedErrorMessage, aOutput.getErrors().get(0).message());
+
+        Mockito.verify(inventoryGateway, Mockito.times(0))
+                .existsBySkus(Mockito.any());
+        Mockito.verify(inventoryGateway, Mockito.times(0)).createInBatch(Mockito.any());
     }
 }

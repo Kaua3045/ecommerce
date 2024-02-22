4dd054cc41c872e1d1be2723125b21015b69dfcc
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-22 18:22:16 -0300

    feat(product-inventory-gateway): add the product inventory gateway with create inventory method

---
+++ application/src/main/java/com/kaua/ecommerce/application/gateways/ProductInventoryGateway.java
@@ -0,0 +1,13 @@
+package com.kaua.ecommerce.application.gateways;
+
+import com.kaua.ecommerce.application.either.Either;
+import com.kaua.ecommerce.domain.validation.handler.NotificationHandler;
+
+import java.util.List;
+
+public interface ProductInventoryGateway {
+
+    Either<NotificationHandler, Void> createInventory(String productId, List<CreateInventoryParams> inventoryParams);
+
+    record CreateInventoryParams(String sku, int quantity) {}
+}

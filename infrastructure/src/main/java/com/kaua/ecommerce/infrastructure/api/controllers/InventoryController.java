b6e4317d7fc097fefd25378c67c12a1b52e7f7a2
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-24 17:49:54 -0300

    feat(inventory-api): add the delete inventory by sku route method implementation

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/api/controllers/InventoryController.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/api/controllers/InventoryController.java
@@ -3,6 +3,7 @@ package com.kaua.ecommerce.infrastructure.api.controllers;
 import com.kaua.ecommerce.application.usecases.inventory.create.commands.CreateInventoryCommand;
 import com.kaua.ecommerce.application.usecases.inventory.create.CreateInventoryUseCase;
 import com.kaua.ecommerce.application.usecases.inventory.delete.clean.CleanInventoriesByProductIdUseCase;
+import com.kaua.ecommerce.application.usecases.inventory.delete.remove.RemoveInventoryBySkuUseCase;
 import com.kaua.ecommerce.domain.inventory.Inventory;
 import com.kaua.ecommerce.infrastructure.api.InventoryAPI;
 import com.kaua.ecommerce.infrastructure.inventory.models.CreateInventoryInput;
@@ -20,13 +21,16 @@ public class InventoryController implements InventoryAPI {
 
     private final CreateInventoryUseCase createInventoryUseCase;
     private final CleanInventoriesByProductIdUseCase cleanInventoriesByProductIdUseCase;
+    private final RemoveInventoryBySkuUseCase removeInventoryBySkuUseCase;
 
     public InventoryController(
             final CreateInventoryUseCase createInventoryUseCase,
-            final CleanInventoriesByProductIdUseCase cleanInventoriesByProductIdUseCase
+            final CleanInventoriesByProductIdUseCase cleanInventoriesByProductIdUseCase,
+            final RemoveInventoryBySkuUseCase removeInventoryBySkuUseCase
     ) {
         this.createInventoryUseCase = createInventoryUseCase;
         this.cleanInventoriesByProductIdUseCase = cleanInventoriesByProductIdUseCase;
+        this.removeInventoryBySkuUseCase = removeInventoryBySkuUseCase;
     }
 
     @Override
@@ -56,6 +60,17 @@ public class InventoryController implements InventoryAPI {
                 Inventory.class,
                 "deleteInventoriesByProductId",
                 productId + " deleted inventories"
+        );
+    }
+
+    @Override
+    public void deleteInventoryBySku(String sku) {
+        this.removeInventoryBySkuUseCase.execute(sku);
+        LogControllerResult.logResult(
+                log,
+                Inventory.class,
+                "deleteInventoryBySku",
+                sku + " deleted inventory"
         );
     }
 }

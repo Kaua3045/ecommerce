59295c6430182e0e6de7b58da705414420192fde
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-25 14:25:24 -0300

    test(product-inventory-gateway): add the delete inventory by skull implementation tests

--- infrastructure/src/test/java/com/kaua/ecommerce/infrastructure/product/ProductInventoryUseCaseGatewayTest.java
+++ infrastructure/src/test/java/com/kaua/ecommerce/infrastructure/product/ProductInventoryUseCaseGatewayTest.java
@@ -3,6 +3,7 @@ package com.kaua.ecommerce.infrastructure.product;
 import com.kaua.ecommerce.application.gateways.ProductInventoryGateway;
 import com.kaua.ecommerce.application.usecases.inventory.create.CreateInventoryUseCase;
 import com.kaua.ecommerce.application.usecases.inventory.delete.clean.CleanInventoriesByProductIdUseCase;
+import com.kaua.ecommerce.application.usecases.inventory.delete.remove.RemoveInventoryBySkuUseCase;
 import com.kaua.ecommerce.domain.Fixture;
 import com.kaua.ecommerce.domain.utils.CommonErrorMessage;
 import com.kaua.ecommerce.infrastructure.IntegrationTest;
@@ -115,7 +116,8 @@ public class ProductInventoryUseCaseGatewayTest {
         final var aMockCleanInventoriesByProductIdUseCase = Mockito.mock(CleanInventoriesByProductIdUseCase.class);
         final var aMockProductInventoryGateway = new ProductInventoryUseCaseGateway(
                 Mockito.mock(CreateInventoryUseCase.class),
-                aMockCleanInventoriesByProductIdUseCase
+                aMockCleanInventoriesByProductIdUseCase,
+                Mockito.mock(RemoveInventoryBySkuUseCase.class)
         );
 
         final var aProductId = "invalid-id";
@@ -125,6 +127,52 @@ public class ProductInventoryUseCaseGatewayTest {
         final var aOutput = aMockProductInventoryGateway.cleanInventoriesByProductId(aProductId);
 
         Assertions.assertTrue(aOutput.isLeft());
-        Assertions.assertEquals("error on clean inventories by product id", aOutput.getLeft().getErrors().get(0).message());
+        Assertions.assertEquals("error on clean inventories by product id %s".formatted(aProductId), aOutput.getLeft().getErrors().get(0).message());
+    }
+
+    @Test
+    void givenAValidSku_whenCallRemoveInventoryBySku_thenShouldRemoveInventory() {
+        final var aProduct = Fixture.Products.book();
+        this.productJpaRepository.saveAndFlush(ProductJpaEntity.toEntity(aProduct));
+
+        final var aProductId = aProduct.getId().getValue();
+        final var aProductSku = aProduct.getAttributes().stream().findFirst().get().getSku();
+        final var aQuantity = 5;
+
+        final var aInventoryParams = new ProductInventoryGateway.CreateInventoryParams(
+                aProductSku,
+                aQuantity
+        );
+
+        this.productInventoryGateway.createInventory(
+                aProductId,
+                List.of(aInventoryParams)
+        );
+
+        Assertions.assertEquals(1, this.inventoryJpaRepository.count());
+
+        final var aOutput = this.productInventoryGateway.deleteInventoryBySku(aProductSku);
+
+        Assertions.assertEquals(0, this.inventoryJpaRepository.count());
+        Assertions.assertTrue(aOutput.isRight());
+    }
+
+    @Test
+    void givenAnInvalidSku_whenCallRemoveInventoryBySku_thenReturnNotificationHandlerWithException() {
+        final var aMockRemoveInventoryBySkuUseCase = Mockito.mock(RemoveInventoryBySkuUseCase.class);
+        final var aMockProductInventoryGateway = new ProductInventoryUseCaseGateway(
+                Mockito.mock(CreateInventoryUseCase.class),
+                Mockito.mock(CleanInventoriesByProductIdUseCase.class),
+                aMockRemoveInventoryBySkuUseCase
+        );
+
+        final var aSku = "invalid-sku";
+
+        Mockito.doThrow(new RuntimeException()).when(aMockRemoveInventoryBySkuUseCase).execute(aSku);
+
+        final var aOutput = aMockProductInventoryGateway.deleteInventoryBySku(aSku);
+
+        Assertions.assertTrue(aOutput.isLeft());
+        Assertions.assertEquals("error on delete inventory by sku %s".formatted(aSku), aOutput.getLeft().getErrors().get(0).message());
     }
 }

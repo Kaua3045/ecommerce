327afa155fa6272c5fb8658b49b5ac9f7e0067ad
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-25 14:22:54 -0300

    feat(remove-product-attributes-usecase): add the delete inventory but on delete product fails does not recovery inventory, in future implement logic to reverse using inventory movement

--- application/src/main/java/com/kaua/ecommerce/application/usecases/product/attributes/remove/DefaultRemoveProductAttributesUseCase.java
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/product/attributes/remove/DefaultRemoveProductAttributesUseCase.java
@@ -6,6 +6,8 @@ import com.kaua.ecommerce.application.exceptions.ProductNotHaveMoreAttributesExc
 import com.kaua.ecommerce.application.exceptions.TransactionFailureException;
 import com.kaua.ecommerce.application.gateways.EventPublisher;
 import com.kaua.ecommerce.application.gateways.ProductGateway;
+import com.kaua.ecommerce.application.gateways.ProductInventoryGateway;
+import com.kaua.ecommerce.domain.exceptions.DomainException;
 import com.kaua.ecommerce.domain.exceptions.NotFoundException;
 import com.kaua.ecommerce.domain.product.Product;
 import com.kaua.ecommerce.domain.product.ProductStatus;
@@ -18,15 +20,18 @@ public class DefaultRemoveProductAttributesUseCase extends RemoveProductAttribut
     private final ProductGateway productGateway;
     private final TransactionManager transactionManager;
     private final EventPublisher eventPublisher;
+    private final ProductInventoryGateway productInventoryGateway;
 
     public DefaultRemoveProductAttributesUseCase(
             final ProductGateway productGateway,
             final TransactionManager transactionManager,
-            final EventPublisher eventPublisher
+            final EventPublisher eventPublisher,
+            final ProductInventoryGateway productInventoryGateway
     ) {
         this.productGateway = Objects.requireNonNull(productGateway);
         this.transactionManager = Objects.requireNonNull(transactionManager);
         this.eventPublisher = Objects.requireNonNull(eventPublisher);
+        this.productInventoryGateway = Objects.requireNonNull(productInventoryGateway);
     }
 
     @Override
@@ -45,6 +50,13 @@ public class DefaultRemoveProductAttributesUseCase extends RemoveProductAttribut
         final var aResultAfterCallRemove = aProduct.removeAttribute(input.sku());
 
         if (aResultAfterCallRemove != null) {
+            final var aResultOnDeleteInventory = this.productInventoryGateway
+                    .deleteInventoryBySku(input.sku());
+
+            if (aResultOnDeleteInventory.isLeft()) {
+                throw DomainException.with(aResultOnDeleteInventory.getLeft().getErrors());
+            }
+
             final var aTransactionResult = this.transactionManager.execute(() -> {
                 final var aUpdateResult = this.productGateway.update(aResultAfterCallRemove);
                 this.eventPublisher.publish(ProductUpdatedEvent.from(aResultAfterCallRemove));
@@ -52,6 +64,11 @@ public class DefaultRemoveProductAttributesUseCase extends RemoveProductAttribut
             });
 
             if (aTransactionResult.isFailure()) {
+                // TODO: precisamos de uma forma para compensar o pagamento
+                // alguma forma de dar rollback, ou buscamos o inventory pra deixar in-memory
+                // ou então usamos o inventory movement para restaurar a versão antiga do inventory
+                // o inventory movement dai tem um IN, OUT e DELETED status, para sabermos quando
+                // um inventory foi removido, e podemos restaurar ele
                 throw TransactionFailureException.with(aTransactionResult.getErrorResult());
             }
         }

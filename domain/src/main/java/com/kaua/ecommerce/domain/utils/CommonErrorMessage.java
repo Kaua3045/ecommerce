a6d5d25099779ae1b951abf0160d09f8917ab2fc
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-26 09:10:36 -0300

    feat(common-error-message): add the null error message

--- domain/src/main/java/com/kaua/ecommerce/domain/utils/CommonErrorMessage.java
+++ domain/src/main/java/com/kaua/ecommerce/domain/utils/CommonErrorMessage.java
@@ -2,10 +2,15 @@ package com.kaua.ecommerce.domain.utils;
 
 public final class CommonErrorMessage {
 
-    private CommonErrorMessage() {}
+    private CommonErrorMessage() {
+    }
 
     public static String nullOrBlank(final String fieldName) {
         return "'" + fieldName + "' should not be null or blank";
+    }
+
+    public static String nullMessage(final String fieldName) {
+        return "'" + fieldName + "' should not be null";
     }
 
     public static String lengthBetween(final String fieldName, final int min, final int max) {

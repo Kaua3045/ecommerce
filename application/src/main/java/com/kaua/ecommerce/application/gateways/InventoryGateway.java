1338e2ca6f6abcd699536ad4eeb1edb0090c0073
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-26 17:03:03 -0300

    feat(inventory-gateway): add the findBySku method

--- application/src/main/java/com/kaua/ecommerce/application/gateways/InventoryGateway.java
+++ application/src/main/java/com/kaua/ecommerce/application/gateways/InventoryGateway.java
@@ -3,6 +3,7 @@ package com.kaua.ecommerce.application.gateways;
 import com.kaua.ecommerce.domain.inventory.Inventory;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 public interface InventoryGateway {
@@ -10,6 +11,8 @@ public interface InventoryGateway {
     Set<Inventory> createInBatch(Set<Inventory> inventories);
 
     List<String> existsBySkus(List<String> skus);
+
+    Optional<Inventory> findBySku(String sku);
 
     void cleanByProductId(String productId);
 

7b2dd048043397e0dfa97e4e9e8b7053211ad1d5
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-26 09:16:57 -0300

    feat(create-inventory-usecase): add the inventory movement gateway to create movements

--- application/src/main/java/com/kaua/ecommerce/application/usecases/inventory/create/DefaultCreateInventoryUseCase.java
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/inventory/create/DefaultCreateInventoryUseCase.java
@@ -1,15 +1,22 @@
 package com.kaua.ecommerce.application.usecases.inventory.create;
 
+import com.kaua.ecommerce.application.adapters.TransactionManager;
 import com.kaua.ecommerce.application.either.Either;
+import com.kaua.ecommerce.application.exceptions.TransactionFailureException;
 import com.kaua.ecommerce.application.gateways.InventoryGateway;
+import com.kaua.ecommerce.application.gateways.InventoryMovementGateway;
 import com.kaua.ecommerce.application.usecases.inventory.create.commands.CreateInventoryCommand;
 import com.kaua.ecommerce.application.usecases.inventory.create.commands.CreateInventoryCommandParams;
 import com.kaua.ecommerce.application.usecases.inventory.create.outputs.CreateInventoryOutput;
 import com.kaua.ecommerce.domain.exceptions.DomainException;
 import com.kaua.ecommerce.domain.inventory.Inventory;
+import com.kaua.ecommerce.domain.inventory.movement.InventoryMovement;
+import com.kaua.ecommerce.domain.inventory.movement.InventoryMovementStatus;
+import com.kaua.ecommerce.domain.utils.CollectionUtils;
 import com.kaua.ecommerce.domain.validation.Error;
 import com.kaua.ecommerce.domain.validation.handler.NotificationHandler;
 
+import java.util.AbstractMap;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -17,9 +24,17 @@ import java.util.stream.Collectors;
 public class DefaultCreateInventoryUseCase extends CreateInventoryUseCase {
 
     private final InventoryGateway inventoryGateway;
+    private final InventoryMovementGateway inventoryMovementGateway;
+    private final TransactionManager transactionManager;
 
-    public DefaultCreateInventoryUseCase(final InventoryGateway inventoryGateway) {
+    public DefaultCreateInventoryUseCase(
+            final InventoryGateway inventoryGateway,
+            final InventoryMovementGateway inventoryMovementGateway,
+            final TransactionManager transactionManager
+    ) {
         this.inventoryGateway = Objects.requireNonNull(inventoryGateway);
+        this.inventoryMovementGateway = Objects.requireNonNull(inventoryMovementGateway);
+        this.transactionManager = Objects.requireNonNull(transactionManager);
     }
 
     @Override
@@ -36,7 +51,16 @@ public class DefaultCreateInventoryUseCase extends CreateInventoryUseCase {
                             aParam.quantity());
                     aInventory.validate(aNotification);
 
-                    return aInventory;
+                    final var aInventoryMovement = InventoryMovement
+                            .newInventoryMovement(
+                                    aInventory.getId(),
+                                    aInventory.getSku(),
+                                    aInventory.getQuantity(),
+                                    InventoryMovementStatus.IN
+                            );
+                    aInventoryMovement.validate(aNotification);
+
+                    return new AbstractMap.SimpleImmutableEntry<>(aInventory, aInventoryMovement);
                 })
                 .collect(Collectors.toSet());
 
@@ -44,9 +68,21 @@ public class DefaultCreateInventoryUseCase extends CreateInventoryUseCase {
             return Either.left(aNotification);
         }
 
-        final var aInventoriesCreateResult = this.inventoryGateway.createInBatch(aInventories);
+        final var aTransactionResult = this.transactionManager.execute(() -> {
+            final var aInventoriesCreateResult = this.inventoryGateway
+                    .createInBatch(CollectionUtils
+                            .mapTo(aInventories, AbstractMap.SimpleImmutableEntry::getKey));
+            this.inventoryMovementGateway
+                    .createInBatch(CollectionUtils
+                            .mapTo(aInventories, AbstractMap.SimpleImmutableEntry::getValue));
+            return aInventoriesCreateResult;
+        });
 
-        return Either.right(CreateInventoryOutput.from(input.productId(), aInventoriesCreateResult));
+        if (aTransactionResult.isFailure()) {
+            throw TransactionFailureException.with(aTransactionResult.getErrorResult());
+        }
+
+        return Either.right(CreateInventoryOutput.from(input.productId(), aTransactionResult.getSuccessResult()));
     }
 
     private Set<CreateInventoryCommandParams> returnWithSkusValid(final Set<CreateInventoryCommandParams> input) {

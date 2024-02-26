71afb96dc43c12cd019c5f135ab7c7b1ca2aa6e2
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-26 17:05:53 -0300

    feat(inventory-jpa-repository): add the findBySku method in jpa repository

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/inventory/persistence/InventoryJpaRepository.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/inventory/persistence/InventoryJpaRepository.java
@@ -4,15 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface InventoryJpaRepository extends JpaRepository<InventoryJpaEntity, String> {
 
     @Query(value = "SELECT i.sku FROM InventoryJpaEntity i WHERE i.sku IN :skus")
     List<String> existsBySkus(List<String> skus);
 
-    boolean existsByProductId(String productId);
+    Optional<InventoryJpaEntity> findBySku(String sku);
 
-    boolean existsBySku(String sku);
+    boolean existsByProductId(String productId);
 
     void deleteAllByProductId(String productId);
 

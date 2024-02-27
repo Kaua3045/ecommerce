a2b2007e0175031fd90a92c5c47084e5e1a61762
Kauã Pereira <zkauacodes@hotmail.com>
2024-02-27 15:53:56 -0300

    test(add-product-attributes-usecase): add to create inventories and publish rollback event on transaction fails tests

--- application/src/test/java/com/kaua/ecommerce/application/usecases/product/attributes/add/AddProductAttributesUseCaseTest.java
+++ application/src/test/java/com/kaua/ecommerce/application/usecases/product/attributes/add/AddProductAttributesUseCaseTest.java
@@ -3,13 +3,16 @@ package com.kaua.ecommerce.application.usecases.product.attributes.add;
 import com.kaua.ecommerce.application.UseCaseTest;
 import com.kaua.ecommerce.application.adapters.TransactionManager;
 import com.kaua.ecommerce.application.adapters.responses.TransactionResult;
+import com.kaua.ecommerce.application.either.Either;
 import com.kaua.ecommerce.application.exceptions.TransactionFailureException;
 import com.kaua.ecommerce.application.gateways.EventPublisher;
 import com.kaua.ecommerce.application.gateways.ProductGateway;
+import com.kaua.ecommerce.application.gateways.ProductInventoryGateway;
 import com.kaua.ecommerce.domain.Fixture;
 import com.kaua.ecommerce.domain.exceptions.DomainException;
 import com.kaua.ecommerce.domain.product.ProductStatus;
 import com.kaua.ecommerce.domain.validation.Error;
+import com.kaua.ecommerce.domain.validation.handler.NotificationHandler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -35,6 +38,9 @@ public class AddProductAttributesUseCaseTest extends UseCaseTest {
     @Mock
     private EventPublisher eventPublisher;
 
+    @Mock
+    private ProductInventoryGateway productInventoryGateway;
+
     @InjectMocks
     private DefaultAddProductAttributesUseCase addProductAttributesUseCase;
 
@@ -44,6 +50,7 @@ public class AddProductAttributesUseCaseTest extends UseCaseTest {
         final var aProductId = aProduct.getId().getValue();
 
         final var aProductAttribute = Fixture.Products.productAttributes(aProduct.getName());
+        final var aQuantity = 5;
 
         final var aProductUpdatedAt = aProduct.getUpdatedAt();
 
@@ -54,10 +61,13 @@ public class AddProductAttributesUseCaseTest extends UseCaseTest {
                         aProductAttribute.getSize().getWeight(),
                         aProductAttribute.getSize().getHeight(),
                         aProductAttribute.getSize().getWidth(),
-                        aProductAttribute.getSize().getDepth()
+                        aProductAttribute.getSize().getDepth(),
+                        aQuantity
                 )));
 
         Mockito.when(productGateway.findById(aProductId)).thenReturn(Optional.of(aProduct));
+        Mockito.when(productInventoryGateway.createInventory(Mockito.any(), Mockito.anyList()))
+                .thenReturn(Either.right(null));
         Mockito.when(productGateway.update(aProduct)).thenAnswer(returnsFirstArg());
         Mockito.when(this.transactionManager.execute(Mockito.any())).thenAnswer(it -> {
             final var aSupplier = it.getArgument(0, Supplier.class);
@@ -71,6 +81,8 @@ public class AddProductAttributesUseCaseTest extends UseCaseTest {
         Assertions.assertEquals(aProductId, aOutput.productId());
 
         Mockito.verify(productGateway, Mockito.times(1)).findById(aProductId);
+        Mockito.verify(productInventoryGateway, Mockito.times(1))
+                .createInventory(Mockito.any(), Mockito.anyList());
         Mockito.verify(productGateway, Mockito.times(1)).update(argThat(aCmd ->
                 Objects.equals(aProductId, aCmd.getId().getValue())
                         && Objects.equals(2, aCmd.getAttributes().size())
@@ -87,6 +99,7 @@ public class AddProductAttributesUseCaseTest extends UseCaseTest {
         final var aProductAttribute = Fixture.Products.productAttributes(aProduct.getName());
 
         final var aProductId = aProduct.getId().getValue();
+        final var aQuantity = 5;
 
         final var expectedErrorMessage = "Product with id %s is deleted".formatted(aProductId);
 
@@ -97,7 +110,8 @@ public class AddProductAttributesUseCaseTest extends UseCaseTest {
                         aProductAttribute.getSize().getWeight(),
                         aProductAttribute.getSize().getHeight(),
                         aProductAttribute.getSize().getWidth(),
-                        aProductAttribute.getSize().getDepth()
+                        aProductAttribute.getSize().getDepth(),
+                        aQuantity
                 )));
 
         Mockito.when(productGateway.findById(aProductId)).thenReturn(Optional.of(aProduct));
@@ -136,11 +150,12 @@ public class AddProductAttributesUseCaseTest extends UseCaseTest {
     }
 
     @Test
-    void givenAValidCommand_whenCallExecuteButThrowsOnPublishEvent_thenShouldThrowTransactionFailureException() {
+    void givenAValidCommand_whenCallExecuteButOnCreateInventoriesThrows_thenShouldThrowDomainException() {
         final var aProduct = Fixture.Products.tshirt();
         final var aProductAttribute = Fixture.Products.productAttributes(aProduct.getName());
 
         final var aProductId = aProduct.getId().getValue();
+        final var aQuantity = 5;
 
         final var aCommand = AddProductAttributesCommand.with(
                 aProductId, List.of(new AddProductAttributesCommandParams(
@@ -149,14 +164,55 @@ public class AddProductAttributesUseCaseTest extends UseCaseTest {
                         aProductAttribute.getSize().getWeight(),
                         aProductAttribute.getSize().getHeight(),
                         aProductAttribute.getSize().getWidth(),
-                        aProductAttribute.getSize().getDepth()
+                        aProductAttribute.getSize().getDepth(),
+                        aQuantity
+                )));
+
+        final var expectedErrorMessage = "Error on create inventories";
+
+        Mockito.when(productGateway.findById(aProductId)).thenReturn(Optional.of(aProduct));
+        Mockito.when(productInventoryGateway.createInventory(Mockito.any(), Mockito.anyList()))
+                .thenReturn(Either.left(NotificationHandler.create(new Error(expectedErrorMessage))));
+
+        final var aException = Assertions.assertThrows(DomainException.class,
+                () -> this.addProductAttributesUseCase.execute(aCommand));
+
+        Assertions.assertNotNull(aException);
+        Assertions.assertEquals(expectedErrorMessage, aException.getErrors().get(0).message());
+
+        Mockito.verify(productGateway, Mockito.times(1)).findById(aProductId);
+        Mockito.verify(productInventoryGateway, Mockito.times(1))
+                .createInventory(Mockito.any(), Mockito.anyList());
+        Mockito.verify(productGateway, Mockito.times(0)).update(Mockito.any());
+    }
+
+    @Test
+    void givenAValidCommand_whenCallExecuteButThrowsOnPublishEvent_thenShouldThrowTransactionFailureException() {
+        final var aProduct = Fixture.Products.tshirt();
+        final var aProductAttribute = Fixture.Products.productAttributes(aProduct.getName());
+
+        final var aProductId = aProduct.getId().getValue();
+        final var aQuantity = 5;
+
+        final var aCommand = AddProductAttributesCommand.with(
+                aProductId, List.of(new AddProductAttributesCommandParams(
+                        aProductAttribute.getColor().getColor(),
+                        aProductAttribute.getSize().getSize(),
+                        aProductAttribute.getSize().getWeight(),
+                        aProductAttribute.getSize().getHeight(),
+                        aProductAttribute.getSize().getWidth(),
+                        aProductAttribute.getSize().getDepth(),
+                        aQuantity
                 )));
 
         final var expectedErrorMessage = "Error on publish event";
 
         Mockito.when(productGateway.findById(aProductId)).thenReturn(Optional.of(aProduct));
+        Mockito.when(productInventoryGateway.createInventory(Mockito.any(), Mockito.anyList()))
+                .thenReturn(Either.right(null));
         Mockito.when(this.transactionManager.execute(Mockito.any())).thenReturn(TransactionResult
                 .failure(new Error(expectedErrorMessage)));
+        Mockito.doNothing().when(this.eventPublisher).publish(Mockito.any());
 
         final var aOutput = Assertions.assertThrows(
                 TransactionFailureException.class,
@@ -165,6 +221,9 @@ public class AddProductAttributesUseCaseTest extends UseCaseTest {
         Assertions.assertEquals(expectedErrorMessage, aOutput.getMessage());
 
         Mockito.verify(productGateway, Mockito.times(1)).findById(aProduct.getId().getValue());
+        Mockito.verify(productInventoryGateway, Mockito.times(1))
+                .createInventory(Mockito.any(), Mockito.anyList());
         Mockito.verify(productGateway, Mockito.times(0)).update(Mockito.any());
+        Mockito.verify(eventPublisher, Mockito.times(1)).publish(Mockito.any());
     }
 }

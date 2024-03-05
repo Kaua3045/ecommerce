c43cd4206525910a114b6af59995b2add79ab509
Kauã Pereira <zkauacodes@hotmail.com>
2024-03-05 08:29:10 -0300

    refactor(either): remove fold methods

--- application/src/main/java/com/kaua/ecommerce/application/either/Either.java
+++ application/src/main/java/com/kaua/ecommerce/application/either/Either.java
@@ -1,10 +1,6 @@
 package com.kaua.ecommerce.application.either;
 
 import java.util.NoSuchElementException;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 public abstract class Either<L, R> {
 
@@ -23,23 +19,6 @@ public abstract class Either<L, R> {
     public abstract L getLeft();
 
     public abstract R getRight();
-
-    public <T> T fold(Function<L, T> leftMapper, Function<R, T> rightMapper, Consumer<Object> logging) {
-        Objects.requireNonNull(leftMapper, "leftMapper is required");
-        Objects.requireNonNull(rightMapper, "rightMapper is required");
-        final Optional<Consumer<Object>> loggingOptional = Optional.ofNullable(logging);
-        if (isLeft()) {
-            loggingOptional.ifPresent(consumer -> consumer.accept(getLeft()));
-            return leftMapper.apply(getLeft());
-        } else {
-            loggingOptional.ifPresent(consumer -> consumer.accept(getRight()));
-            return rightMapper.apply(getRight());
-        }
-    }
-
-    public <T> T fold(Function<L, T> leftMapper, Function<R, T> rightMapper) {
-        return fold(leftMapper, rightMapper, null);
-    }
 
     public static class BaseMethods<L, R> extends Either<L, R> {
 

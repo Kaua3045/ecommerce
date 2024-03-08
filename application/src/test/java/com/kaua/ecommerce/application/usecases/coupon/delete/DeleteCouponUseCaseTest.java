8f18adb4cba2250d3cbe6f3e853351b08ae41a0b
Kauã Pereira <zkauacodes@hotmail.com>
2024-03-08 13:25:25 -0300

    test(delete-coupon-usecase): add the delete coupon usecase implementation tests

---
+++ application/src/test/java/com/kaua/ecommerce/application/usecases/coupon/delete/DeleteCouponUseCaseTest.java
@@ -0,0 +1,69 @@
+package com.kaua.ecommerce.application.usecases.coupon.delete;
+
+import com.kaua.ecommerce.application.UseCaseTest;
+import com.kaua.ecommerce.application.adapters.TransactionManager;
+import com.kaua.ecommerce.application.adapters.responses.TransactionResult;
+import com.kaua.ecommerce.application.exceptions.TransactionFailureException;
+import com.kaua.ecommerce.application.gateways.CouponGateway;
+import com.kaua.ecommerce.application.gateways.CouponSlotGateway;
+import com.kaua.ecommerce.domain.Fixture;
+import com.kaua.ecommerce.domain.validation.Error;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.function.Supplier;
+
+public class DeleteCouponUseCaseTest extends UseCaseTest {
+
+    @Mock
+    private CouponGateway couponGateway;
+
+    @Mock
+    private CouponSlotGateway couponSlotGateway;
+
+    @Mock
+    private TransactionManager transactionManager;
+
+    @InjectMocks
+    private DefaultDeleteCouponUseCase deleteCouponUseCase;
+
+    @Test
+    void givenAValidId_whenCallExecute_thenShouldDeleteCoupon() {
+        final var aCoupon = Fixture.Coupons.limitedCouponActivated();
+        final var aId = aCoupon.getId().getValue();
+
+        Mockito.doNothing().when(couponGateway).delete(aId);
+        Mockito.doNothing().when(couponSlotGateway).deleteAllByCouponId(aId);
+        Mockito.when(transactionManager.execute(Mockito.any())).thenAnswer(it -> {
+            final var aSupplier = it.getArgument(0, Supplier.class);
+            return TransactionResult.success(aSupplier.get());
+        });
+
+        Assertions.assertDoesNotThrow(() -> this.deleteCouponUseCase.execute(aId));
+
+        Mockito.verify(couponGateway, Mockito.times(1)).delete(aId);
+        Mockito.verify(couponSlotGateway, Mockito.times(1)).deleteAllByCouponId(aId);
+    }
+
+    @Test
+    void givenAnErrorOnTransaction_whenCallExecute_thenShouldThrowTransactionFailureException() {
+        final var aCoupon = Fixture.Coupons.limitedCouponActivated();
+        final var aId = aCoupon.getId().getValue();
+
+        final var expectedErrorMessage = "error on transaction";
+
+        Mockito.when(transactionManager.execute(Mockito.any())).thenAnswer(it ->
+                TransactionResult.failure(new Error(expectedErrorMessage)));
+
+        final var aOutput = Assertions.assertThrows(TransactionFailureException.class,
+                () -> this.deleteCouponUseCase.execute(aId));
+
+        Assertions.assertEquals(expectedErrorMessage, aOutput.getMessage());
+
+        Mockito.verify(couponGateway, Mockito.times(0)).delete(aId);
+        Mockito.verify(couponSlotGateway, Mockito.times(0)).deleteAllByCouponId(aId);
+    }
+}

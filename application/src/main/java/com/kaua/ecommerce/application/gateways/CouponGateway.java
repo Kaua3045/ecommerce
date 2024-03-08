3f4cc80073d3d963d9b0523c624c4cb749a49c97
Kauã Pereira <zkauacodes@hotmail.com>
2024-03-08 10:54:01 -0300

    feat(coupon-gateway): add the update and find by id methods

--- application/src/main/java/com/kaua/ecommerce/application/gateways/CouponGateway.java
+++ application/src/main/java/com/kaua/ecommerce/application/gateways/CouponGateway.java
@@ -2,9 +2,15 @@ package com.kaua.ecommerce.application.gateways;
 
 import com.kaua.ecommerce.domain.coupon.Coupon;
 
+import java.util.Optional;
+
 public interface CouponGateway {
 
     Coupon create(Coupon coupon);
 
     boolean existsByCode(String code);
+
+    Coupon update(Coupon coupon);
+
+    Optional<Coupon> findById(String id);
 }

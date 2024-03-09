ce6073ed6ac887606d910c3867522cbc0744bc43
Kauã Pereira <zkauacodes@hotmail.com>
2024-03-09 11:02:09 -0300

    feat(validation-coupon-usecase): add the validation coupon usecase implementation and output class

---
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/coupon/validate/ValidateCouponUseCase.java
@@ -0,0 +1,6 @@
+package com.kaua.ecommerce.application.usecases.coupon.validate;
+
+import com.kaua.ecommerce.application.UseCase;
+
+public abstract class ValidateCouponUseCase extends UseCase<ValidateCouponOutput, String> {
+}

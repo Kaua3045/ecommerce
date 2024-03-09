12e346d0115d850411e131d0720070f42ab2875a
Kauã Pereira <zkauacodes@hotmail.com>
2024-03-09 17:02:25 -0300

    feat(coupon-slot-jpa-entity-repository): add the delete first slot by coupon id method with custom query

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/coupon/slot/persistence/CouponSlotJpaEntityRepository.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/coupon/slot/persistence/CouponSlotJpaEntityRepository.java
@@ -1,10 +1,17 @@
 package com.kaua.ecommerce.infrastructure.coupon.slot.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CouponSlotJpaEntityRepository extends JpaRepository<CouponSlotJpaEntity, String> {
 
     boolean existsByCouponId(String couponId);
 
     void deleteAllByCouponId(String couponId);
+
+    @Modifying
+    @Query(value = "DELETE FROM coupons_slots c WHERE c.coupon_id = :couponId LIMIT 1", nativeQuery = true)
+    int deleteFirstSlotByCouponId(@Param("couponId") String couponId);
 }

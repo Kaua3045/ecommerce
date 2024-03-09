2c8e1d273f93bb9ffe05321fe6ccfe505af799aa
Kauã Pereira <zkauacodes@hotmail.com>
2024-03-09 20:09:27 -0300

    feat(specification-utils): add the specification utils class to make like and equal search

---
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/utils/SpecificationUtils.java
@@ -0,0 +1,17 @@
+package com.kaua.ecommerce.infrastructure.utils;
+
+import org.springframework.data.jpa.domain.Specification;
+
+public final class SpecificationUtils {
+
+    private SpecificationUtils() {
+    }
+
+    public static <T> Specification<T> like(final String property, final String value) {
+        return (root, query, cb) -> cb.like(cb.upper(root.get(property)), "%" + value.toUpperCase() + "%");
+    }
+
+    public static <T> Specification<T> equal(final String property, final String value) {
+        return (root, query, cb) -> cb.equal(root.get(property), value);
+    }
+}

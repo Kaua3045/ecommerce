ed32a18674e6b5a9b6b87986615cf62d3a885a23
Kauã Pereira <zkauacodes@hotmail.com>
2024-03-10 11:09:20 -0300

    feat(inventory-api-presenter): add the get inventory response present method

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/inventory/presenter/InventoryApiPresenter.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/inventory/presenter/InventoryApiPresenter.java
@@ -1,6 +1,8 @@
 package com.kaua.ecommerce.infrastructure.inventory.presenter;
 
+import com.kaua.ecommerce.application.usecases.inventory.retrieve.get.GetInventoryBySkuOutput;
 import com.kaua.ecommerce.application.usecases.inventory.retrieve.list.ListInventoriesByProductIdOutput;
+import com.kaua.ecommerce.infrastructure.inventory.models.GetInventoryResponse;
 import com.kaua.ecommerce.infrastructure.inventory.models.ListInventoriesResponse;
 
 public final class InventoryApiPresenter {
@@ -16,6 +18,18 @@ public final class InventoryApiPresenter {
                 aOutput.quantity(),
                 aOutput.createdAt(),
                 aOutput.updatedAt()
+        );
+    }
+
+    public static GetInventoryResponse present(final GetInventoryBySkuOutput aOutput) {
+        return new GetInventoryResponse(
+                aOutput.id(),
+                aOutput.productId(),
+                aOutput.sku(),
+                aOutput.quantity(),
+                aOutput.createdAt(),
+                aOutput.updatedAt(),
+                aOutput.version()
         );
     }
 }

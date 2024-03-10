66d77d177accac16fb4e9ecfa69066660855f6f0
Kauã Pereira <zkauacodes@hotmail.com>
2024-03-10 12:45:38 -0300

    feat(coupon-jpa-entity-repository): add the findAll with specification method

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/coupon/persistence/CouponJpaEntityRepository.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/coupon/persistence/CouponJpaEntityRepository.java
@@ -1,5 +1,8 @@
 package com.kaua.ecommerce.infrastructure.coupon.persistence;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +12,6 @@ public interface CouponJpaEntityRepository extends JpaRepository<CouponJpaEntity
     boolean existsByCode(String code);
 
     Optional<CouponJpaEntity> findByCode(String code);
+
+    Page<CouponJpaEntity> findAll(Specification<CouponJpaEntity> whereClause, Pageable pageable);
 }

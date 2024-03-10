a860d71338cb1307eca04bc9bd9e203f4f808e96
Kauã Pereira <zkauacodes@hotmail.com>
2024-03-10 15:03:12 -0300

    test(search-query): add the period param on create searh query tests

--- domain/src/test/java/com/kaua/ecommerce/domain/pagination/SearchQueryTest.java
+++ domain/src/test/java/com/kaua/ecommerce/domain/pagination/SearchQueryTest.java
@@ -21,5 +21,25 @@ public class SearchQueryTest extends UnitTest {
         Assertions.assertEquals(terms, aQuery.terms());
         Assertions.assertEquals(sort, aQuery.sort());
         Assertions.assertEquals(direction, aQuery.direction());
+        Assertions.assertNull(aQuery.period());
+    }
+
+    @Test
+    void givenAValidValues_whenCallNewSearchQueryWithPeriod_shouldReturnASearchQueryInstance() {
+        final var page = 0;
+        final var perPage = 10;
+        final var terms = "";
+        final var sort = "createdAt";
+        final var direction = "asc";
+        final var period = new Period("2021-01-01T00:00:00Z", "2021-01-31T23:59:59Z");
+
+        final var aQuery = new SearchQuery(page, perPage, terms, sort, direction, period);
+
+        Assertions.assertEquals(page, aQuery.page());
+        Assertions.assertEquals(perPage, aQuery.perPage());
+        Assertions.assertEquals(terms, aQuery.terms());
+        Assertions.assertEquals(sort, aQuery.sort());
+        Assertions.assertEquals(direction, aQuery.direction());
+        Assertions.assertEquals(period, aQuery.period());
     }
 }

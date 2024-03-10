51d976611adcfe348ad373e3f5cd7b8905d9d16f
Kauã Pereira <zkauacodes@hotmail.com>
2024-03-10 15:02:42 -0300

    feat(search-query): add the period param

--- domain/src/main/java/com/kaua/ecommerce/domain/pagination/SearchQuery.java
+++ domain/src/main/java/com/kaua/ecommerce/domain/pagination/SearchQuery.java
@@ -1,10 +1,17 @@
 package com.kaua.ecommerce.domain.pagination;
 
+import java.util.Optional;
+
 public record SearchQuery(
         int page,
         int perPage,
         String terms,
         String sort,
-        String direction
+        String direction,
+        Period period
 ) {
+
+    public SearchQuery(int page, int perPage, String terms, String sort, String direction) {
+        this(page, perPage, terms, sort, direction, null);
+    }
 }

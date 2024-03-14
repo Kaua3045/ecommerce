d3d9c7c155ef2b3d8ccfdbe8a7288252e52819dd
Kauã Pereira <zkauacodes@hotmail.com>
2024-03-14 08:52:37 -0300

    feat(list-freights-by-cep-usecase): add the list freights by cep usecase implementation

---
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/freight/list/DefaultListFreightsByCepUseCase.java
@@ -0,0 +1,30 @@
+package com.kaua.ecommerce.application.usecases.freight.list;
+
+import com.kaua.ecommerce.application.gateways.FreightGateway;
+import com.kaua.ecommerce.application.gateways.commands.ListFreightsCommand;
+import com.kaua.ecommerce.domain.freight.Freight;
+
+import java.util.List;
+import java.util.Objects;
+
+public class DefaultListFreightsByCepUseCase extends ListFreightsByCepUseCase {
+
+    private final FreightGateway freightGateway;
+
+    public DefaultListFreightsByCepUseCase(final FreightGateway freightGateway) {
+        this.freightGateway = Objects.requireNonNull(freightGateway);
+    }
+
+    @Override
+    public List<Freight> execute(ListFreightsByCepCommand input) {
+        final var aListFreightsCommand = ListFreightsCommand.with(
+                input.cep(),
+                input.height(),
+                input.width(),
+                input.length(),
+                input.weight()
+        );
+
+        return this.freightGateway.listFreights(aListFreightsCommand);
+    }
+}
---
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/freight/list/ListFreightsByCepUseCase.java
@@ -0,0 +1,9 @@
+package com.kaua.ecommerce.application.usecases.freight.list;
+
+import com.kaua.ecommerce.application.UseCase;
+import com.kaua.ecommerce.domain.freight.Freight;
+
+import java.util.List;
+
+public abstract class ListFreightsByCepUseCase extends UseCase<List<Freight>, ListFreightsByCepCommand> {
+}

54d458cc53bc063f38140fc1927ff8119e6b425d
Kauã Pereira <zkauacodes@hotmail.com>
2024-03-24 10:26:38 -0300

    feat(order-api): add the create order route method implementation

---
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/api/controllers/OrderController.java
@@ -0,0 +1,42 @@
+package com.kaua.ecommerce.infrastructure.api.controllers;
+
+import com.kaua.ecommerce.application.usecases.order.create.CreateOrderUseCase;
+import com.kaua.ecommerce.domain.order.Order;
+import com.kaua.ecommerce.infrastructure.api.OrderAPI;
+import com.kaua.ecommerce.infrastructure.order.models.CreateOrderInput;
+import com.kaua.ecommerce.infrastructure.utils.LogControllerResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController implements OrderAPI {
+
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
+    private final CreateOrderUseCase createOrderUseCase;
+
+    public OrderController(final CreateOrderUseCase createOrderUseCase) {
+        this.createOrderUseCase = createOrderUseCase;
+    }
+
+    @Override
+    public ResponseEntity<?> createOrder(CreateOrderInput body) {
+        final var aCommand = body.toCommand();
+
+        final var aResult = this.createOrderUseCase.execute(aCommand);
+
+        LogControllerResult.logResult(
+                log,
+                Order.class,
+                "createOrder",
+                aResult
+        );
+
+        return aResult.isLeft()
+                ? ResponseEntity.unprocessableEntity().body(aResult.getLeft())
+                : ResponseEntity.status(HttpStatus.CREATED).body(aResult.getRight());
+    }
+}

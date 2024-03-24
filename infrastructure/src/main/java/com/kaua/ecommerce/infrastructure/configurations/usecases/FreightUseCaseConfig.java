54aee8a925107e91ce9a8cf9aa8b25f9162826d2
Kauã Pereira <zkauacodes@hotmail.com>
2024-03-24 10:21:51 -0300

    feat(freight-usecase-config): add the calculate freight usecase bean config

--- infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/configurations/usecases/FreightUseCaseConfig.java
+++ infrastructure/src/main/java/com/kaua/ecommerce/infrastructure/configurations/usecases/FreightUseCaseConfig.java
@@ -1,6 +1,8 @@
 package com.kaua.ecommerce.infrastructure.configurations.usecases;
 
 import com.kaua.ecommerce.application.gateways.FreightGateway;
+import com.kaua.ecommerce.application.usecases.freight.calculate.CalculateFreightUseCase;
+import com.kaua.ecommerce.application.usecases.freight.calculate.DefaultCalculateFreightUseCase;
 import com.kaua.ecommerce.application.usecases.freight.list.DefaultListFreightsByCepUseCase;
 import com.kaua.ecommerce.application.usecases.freight.list.ListFreightsByCepUseCase;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +17,11 @@ public class FreightUseCaseConfig {
 
     public FreightUseCaseConfig(final FreightGateway freightGateway) {
         this.freightGateway = Objects.requireNonNull(freightGateway);
+    }
+
+    @Bean
+    public CalculateFreightUseCase calculateFreightUseCase() {
+        return new DefaultCalculateFreightUseCase(this.freightGateway);
     }
 
     @Bean

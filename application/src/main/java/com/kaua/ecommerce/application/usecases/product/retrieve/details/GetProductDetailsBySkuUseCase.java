6b656db09d2df77cb5867057f3b437fd495c6fc7
Kauã Pereira <zkauacodes@hotmail.com>
2024-03-28 13:15:17 -0300

    feat(get-product-details-by-sku-usecase): add the get product details by sku usecase implementation

---
+++ application/src/main/java/com/kaua/ecommerce/application/usecases/product/retrieve/details/GetProductDetailsBySkuUseCase.java
@@ -0,0 +1,7 @@
+package com.kaua.ecommerce.application.usecases.product.retrieve.details;
+
+import com.kaua.ecommerce.application.UseCase;
+import com.kaua.ecommerce.application.gateways.responses.ProductDetails;
+
+public abstract class GetProductDetailsBySkuUseCase extends UseCase<ProductDetails, String> {
+}
